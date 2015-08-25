f739fdd19a104a4cf519246c41e964b69f609c69
Luke Hutchison <luke.hutch@gmail.com>
2015-08-25 06:20:03 -0700

    Refactoring

---
+++ src/main/java/io/github/lukehutch/fastclasspathscanner/classgraph/ClassfileBinaryParser.java
@@ -0,0 +1,369 @@
+package io.github.lukehutch.fastclasspathscanner.classgraph;
+
+import io.github.lukehutch.fastclasspathscanner.matchprocessor.StaticFinalFieldMatchProcessor;
+import io.github.lukehutch.fastclasspathscanner.utils.Log;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+class ClassfileBinaryParser {
+    ClassGraphBuilder classGraphBuilder;
+
+    /**
+     * Names of classes encountered so far during a scan. If the same classname is encountered more than once, the
+     * second and subsequent instances are ignored, because they are masked by the earlier occurrence in the
+     * classpath.
+     */
+    private final HashSet<String> classesEncounteredSoFarDuringScan = new HashSet<>();
+
+    /** Clear all the data structures to be ready for another scan. */
+    public void reset() {
+        classesEncounteredSoFarDuringScan.clear();
+    }
+
+    /**
+     * A map from classname, to static final field name, to a StaticFinalFieldMatchProcessor that should be called
+     * if that class name and static final field name is encountered during scan.
+     */
+    private final HashMap<String, HashMap<String, StaticFinalFieldMatchProcessor>> //
+    classNameToStaticFieldnameToMatchProcessor = new HashMap<>();
+
+    /**
+     * Add a StaticFinalFieldMatchProcessor that should be called if a static final field with the given name is
+     * encountered in a class with the given fully-qualified classname while reading a classfile header.
+     */
+    public void addStaticFinalFieldProcessor(String className, String fieldName,
+            StaticFinalFieldMatchProcessor staticFinalFieldMatchProcessor) {
+        HashMap<String, StaticFinalFieldMatchProcessor> fieldNameToMatchProcessor = //
+        classNameToStaticFieldnameToMatchProcessor.get(className);
+        if (fieldNameToMatchProcessor == null) {
+            classNameToStaticFieldnameToMatchProcessor.put(className, fieldNameToMatchProcessor = new HashMap<>(2));
+        }
+        fieldNameToMatchProcessor.put(fieldName, staticFinalFieldMatchProcessor);
+    }
+
+    public ClassfileBinaryParser(ClassGraphBuilder classGraphBuilder) {
+        this.classGraphBuilder = classGraphBuilder;
+    }
+
+    // -------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Read annotation entry from classfile.
+     */
+    private String readAnnotation(final DataInputStream inp, final Object[] constantPool) throws IOException {
+        final String annotationFieldDescriptor = readRefdString(inp, constantPool);
+        String annotationClassName;
+        if (annotationFieldDescriptor.charAt(0) == 'L'
+                && annotationFieldDescriptor.charAt(annotationFieldDescriptor.length() - 1) == ';') {
+            // Lcom/xyz/Annotation; -> com.xyz.Annotation
+            annotationClassName = annotationFieldDescriptor.substring(1, annotationFieldDescriptor.length() - 1)
+                    .replace('/', '.');
+        } else {
+            // Should not happen
+            annotationClassName = annotationFieldDescriptor;
+        }
+        final int numElementValuePairs = inp.readUnsignedShort();
+        for (int i = 0; i < numElementValuePairs; i++) {
+            inp.skipBytes(2); // element_name_index
+            readAnnotationElementValue(inp, constantPool);
+        }
+        return annotationClassName;
+    }
+
+    /**
+     * Read annotation element value from classfile.
+     */
+    private void readAnnotationElementValue(final DataInputStream inp, final Object[] constantPool)
+            throws IOException {
+        final int tag = inp.readUnsignedByte();
+        switch (tag) {
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'F':
+        case 'I':
+        case 'J':
+        case 'S':
+        case 'Z':
+        case 's':
+            // const_value_index
+            inp.skipBytes(2);
+            break;
+        case 'e':
+            // enum_const_value
+            inp.skipBytes(4);
+            break;
+        case 'c':
+            // class_info_index
+            inp.skipBytes(2);
+            break;
+        case '@':
+            // Complex (nested) annotation
+            readAnnotation(inp, constantPool);
+            break;
+        case '[':
+            // array_value
+            final int count = inp.readUnsignedShort();
+            for (int l = 0; l < count; ++l) {
+                // Nested annotation element value
+                readAnnotationElementValue(inp, constantPool);
+            }
+            break;
+        default:
+            // System.err.println("Invalid annotation element type tag: 0x" + Integer.toHexString(tag));
+            break;
+        }
+    }
+
+    /**
+     * Read as usigned short constant pool reference, then look up the string in the constant pool.
+     */
+    private static String readRefdString(final DataInputStream inp, final Object[] constantPool) //
+            throws IOException {
+        return (String) constantPool[inp.readUnsignedShort()];
+    }
+
+    /**
+     * Directly examine contents of classfile binary header.
+     * 
+     * @param verbose
+     */
+    public void readClassInfoFromClassfileHeader(final InputStream inputStream, boolean verbose) //
+            throws IOException {
+        final DataInputStream inp = new DataInputStream(new BufferedInputStream(inputStream, 1024));
+
+        // Magic
+        if (inp.readInt() != 0xCAFEBABE) {
+            // Not classfile
+            return;
+        }
+
+        // Minor version
+        inp.readUnsignedShort();
+        // Major version
+        inp.readUnsignedShort();
+
+        // Constant pool count (1-indexed, zeroth entry not used)
+        final int cpCount = inp.readUnsignedShort();
+        // Constant pool
+        final Object[] constantPool = new Object[cpCount];
+        final int[] indirectStringRef = new int[cpCount];
+        Arrays.fill(indirectStringRef, -1);
+        for (int i = 1; i < cpCount; ++i) {
+            final int tag = inp.readUnsignedByte();
+            switch (tag) {
+            case 1: // Modified UTF8
+                constantPool[i] = inp.readUTF();
+                break;
+            case 3: // int, short, char, byte, boolean are all represented by Constant_INTEGER
+                constantPool[i] = inp.readInt();
+                break;
+            case 4: // float
+                constantPool[i] = inp.readFloat();
+                break;
+            case 5: // long
+                constantPool[i] = inp.readLong();
+                i++; // double slot
+                break;
+            case 6: // double
+                constantPool[i] = inp.readDouble();
+                i++; // double slot
+                break;
+            case 7: // Class
+            case 8: // String
+                // Forward or backward indirect reference to a modified UTF8 entry
+                indirectStringRef[i] = inp.readUnsignedShort();
+                break;
+            case 9: // field ref
+            case 10: // method ref
+            case 11: // interface ref
+            case 12: // name and type
+                inp.skipBytes(4); // two shorts
+                break;
+            case 15: // method handle
+                inp.skipBytes(3);
+                break;
+            case 16: // method type
+                inp.skipBytes(2);
+                break;
+            case 18: // invoke dynamic
+                inp.skipBytes(4);
+                break;
+            default:
+                // System.err.println("Unkown tag value for constant pool entry: " + tag);
+                break;
+            }
+        }
+        // Resolve indirection of string references now that all the strings have been read
+        // (allows forward references to strings before they have been encountered)
+        for (int i = 1; i < cpCount; i++) {
+            if (indirectStringRef[i] >= 0) {
+                constantPool[i] = constantPool[indirectStringRef[i]];
+            }
+        }
+
+        // Access flags
+        final int flags = inp.readUnsignedShort();
+        final boolean isInterface = (flags & 0x0200) != 0;
+        final boolean isAnnotation = (flags & 0x2000) != 0;
+
+        // The fully-qualified class name of this class, with slashes replaced with dots
+        final String className = readRefdString(inp, constantPool).replace('/', '.');
+        if (className.equals("java.lang.Object")) {
+            // java.lang.Object doesn't have a superclass to be linked to, can simply return
+            return;
+        }
+
+        // Determine if this fully-qualified class name has already been encountered during this scan
+        if (!classesEncounteredSoFarDuringScan.add(className)) {
+            // If so, skip this classfile, because the earlier class with the same name as this one
+            // occurred earlier on the classpath, so it masks this one.
+            return;
+        }
+
+        // Superclass name, with slashes replaced with dots
+        final String superclassName = readRefdString(inp, constantPool).replace('/', '.');
+
+        // Look up static field name match processors given class name 
+        final HashMap<String, StaticFinalFieldMatchProcessor> staticFieldnameToMatchProcessor //
+        = classNameToStaticFieldnameToMatchProcessor.get(className);
+
+        // Interfaces
+        final int interfaceCount = inp.readUnsignedShort();
+        final ArrayList<String> interfaces = interfaceCount > 0 ? new ArrayList<String>(interfaceCount) : null;
+        for (int i = 0; i < interfaceCount; i++) {
+            interfaces.add(readRefdString(inp, constantPool).replace('/', '.'));
+        }
+
+        // Fields
+        final int fieldCount = inp.readUnsignedShort();
+        for (int i = 0; i < fieldCount; i++) {
+            final int accessFlags = inp.readUnsignedShort();
+            // See http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.6
+            final boolean isStaticFinal = (accessFlags & 0x0018) == 0x0018;
+            final String fieldName = readRefdString(inp, constantPool);
+            final StaticFinalFieldMatchProcessor staticFinalFieldMatchProcessor //
+            = staticFieldnameToMatchProcessor != null ? staticFieldnameToMatchProcessor.get(fieldName) : null;
+            final String descriptor = readRefdString(inp, constantPool);
+            final int attributesCount = inp.readUnsignedShort();
+            if (!isStaticFinal && staticFinalFieldMatchProcessor != null) {
+                // Requested to match a field that is not static or not final
+                System.err.println(StaticFinalFieldMatchProcessor.class.getSimpleName()
+                        + ": cannot match requested field " + className + "." + fieldName
+                        + " because it is either not static or not final");
+            } else if (!isStaticFinal || staticFinalFieldMatchProcessor == null) {
+                // Not matching this static final field, just skip field attributes rather than parsing them
+                for (int j = 0; j < attributesCount; j++) {
+                    inp.skipBytes(2); // attribute_name_index
+                    final int attributeLength = inp.readInt();
+                    inp.skipBytes(attributeLength);
+                }
+            } else {
+                // Look for static final fields that match one of the requested names,
+                // and that are initialized with a constant value
+                boolean foundConstantValue = false;
+                for (int j = 0; j < attributesCount; j++) {
+                    final String attributeName = readRefdString(inp, constantPool);
+                    final int attributeLength = inp.readInt();
+                    if (attributeName.equals("ConstantValue")) {
+                        // http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.7.2
+                        Object constValue = constantPool[inp.readUnsignedShort()];
+                        // byte, char, short and boolean constants are all stored as 4-byte int
+                        // values -- coerce and wrap in the proper wrapper class with autoboxing
+                        switch (descriptor) {
+                        case "B":
+                            // Convert byte store in Integer to Byte
+                            constValue = ((Integer) constValue).byteValue();
+                            break;
+                        case "C":
+                            // Convert char stored in Integer to Character
+                            constValue = (char) ((Integer) constValue).intValue();
+                            break;
+                        case "S":
+                            // Convert char stored in Integer to Short
+                            constValue = ((Integer) constValue).shortValue();
+                            break;
+                        case "Z":
+                            // Convert char stored in Integer to Boolean
+                            constValue = ((Integer) constValue).intValue() != 0;
+                            break;
+                        case "I":
+                        case "J":
+                        case "F":
+                        case "D":
+                        case "Ljava.lang.String;":
+                            // Field is int, long, float, double or String => object is already in correct
+                            // wrapper type (Integer, Long, Float, Double or String), nothing to do
+                            break;
+                        default:
+                            // Should never happen:
+                            // constant values can only be stored as an int, long, float, double or String
+                            break;
+                        }
+                        // Call static final field match processor
+                        if (verbose) {
+                            Log.log("Found static final field " + className + "." + fieldName + " = " + constValue);
+                        }
+                        staticFinalFieldMatchProcessor.processMatch(className, fieldName, constValue);
+                        foundConstantValue = true;
+                    } else {
+                        inp.skipBytes(attributeLength);
+                    }
+                    if (!foundConstantValue) {
+                        System.err.println(StaticFinalFieldMatchProcessor.class.getSimpleName()
+                                + ": Requested static final field " + className + "." + fieldName
+                                + "is not initialized with a constant literal value, so there is no "
+                                + "initializer value in the constant pool of the classfile");
+                    }
+                }
+            }
+        }
+
+        // Methods
+        final int methodCount = inp.readUnsignedShort();
+        for (int i = 0; i < methodCount; i++) {
+            inp.skipBytes(6); // access_flags, name_index, descriptor_index
+            final int attributesCount = inp.readUnsignedShort();
+            for (int j = 0; j < attributesCount; j++) {
+                inp.skipBytes(2); // attribute_name_index
+                final int attributeLength = inp.readInt();
+                inp.skipBytes(attributeLength);
+            }
+        }
+
+        // Attributes (including class annotations)
+        int attributesCount = inp.readUnsignedShort();
+        for (int i = 0; i < attributesCount; i++) {
+            final String attributeName = readRefdString(inp, constantPool);
+            final int attributeLength = inp.readInt();
+            if ("RuntimeVisibleAnnotations".equals(attributeName)) {
+                final int annotationCount = inp.readUnsignedShort();
+                for (int m = 0; m < annotationCount; m++) {
+                    final String annotationName = readAnnotation(inp, constantPool);
+                    // Ignore java.lang.annotation annotations (Target/Retention/Documented etc.)
+                    if (!annotationName.startsWith("java.lang.annotation.")) {
+                        classGraphBuilder.linkAnnotation(annotationName, className, //
+                                /* classIsAnnotation = */isAnnotation);
+                    }
+                }
+            } else {
+                inp.skipBytes(attributeLength);
+            }
+        }
+
+        if (isAnnotation) {
+            // Class is itself an annotation class. Handled inside classGraphBuilder.linkAnnotation().
+        } else if (isInterface) {
+            classGraphBuilder.linkInterface(/* superInterfaces = */interfaces, /* interfaceName = */className);
+        } else {
+            classGraphBuilder.linkClass(superclassName, interfaces, className);
+        }
+    }
+}
