d9996175b3aedc71e35093e7bc500ae1c76bdb06
Luke Hutchison <luke.hutch@gmail.com>
2015-11-22 22:12:53 -0800

    Remove Scala test support for now (the Scala entries in pom.xml were causing problems)

--- src/test/java/io/github/lukehutch/fastclasspathscanner/outside/Outside.java
+++
@@ -1,4 +0,0 @@
-package io.github.lukehutch.fastclasspathscanner.outside;
-
-public interface Outside {
-}
--- src/test/java/io/github/lukehutch/fastclasspathscanner/whitelisted/ImplementsOutside.java
+++
@@ -1,4 +0,0 @@
-package io.github.lukehutch.fastclasspathscanner.whitelisted;
-
-public class ImplementsOutside implements io.github.lukehutch.fastclasspathscanner.outside.Outside{
-}
