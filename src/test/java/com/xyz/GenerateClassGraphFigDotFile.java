57ee2deb49156077471be86f41f067f20b9e18a9
Luke Hutchison <luke.hutch@gmail.com>
2016-07-17 20:50:15 -0700

    Index all fields

--- src/test/java/com/xyz/GenerateClassGraphFigDotFile.java
+++ src/test/java/com/xyz/GenerateClassGraphFigDotFile.java
@@ -4,6 +4,7 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 
 public class GenerateClassGraphFigDotFile {
     public static void main(final String[] args) {
-        System.out.println(new FastClasspathScanner("com.xyz.fig").scan().generateClassGraphDotFile(9.2f, 8.0f));
+        System.out.println(new FastClasspathScanner("com.xyz.fig").ignoreFieldVisibility().enableFieldTypeIndexing()
+                .scan().generateClassGraphDotFile(9.2f, 8.0f));
     }
 }
