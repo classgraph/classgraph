1bde957232fb31fd1cf5e7b2fe9333a8528a54a8
Luke Hutchison <luke.hutch@gmail.com>
2016-07-17 21:46:56 -0700

    Allow opening of manifest from existing ZipFile

--- src/main/java/io/github/lukehutch/fastclasspathscanner/utils/FastManifestParser.java
+++ src/main/java/io/github/lukehutch/fastclasspathscanner/utils/FastManifestParser.java
@@ -15,6 +15,77 @@ public class FastManifestParser {
     public boolean isSystemJar;
     public String classPath;
 
+    /** Parse the manifest file. */
+    private void parseManifest(final ZipFile jarFile, final ThreadLog log) throws IOException {
+        final ZipEntry zipEntry = jarFile.getEntry("META-INF/MANIFEST.MF");
+        if (zipEntry != null) {
+            try (InputStream inputStream = jarFile.getInputStream(zipEntry)) {
+                final ByteArrayOutputStream byteBuf = new ByteArrayOutputStream();
+                byteBuf.write('\n');
+                final byte[] data = new byte[16384];
+                for (int bytesRead; (bytesRead = inputStream.read(data, 0, data.length)) != -1;) {
+                    byteBuf.write(data, 0, bytesRead);
+                }
+                byteBuf.flush();
+                final String manifest = byteBuf.toString("UTF-8");
+                final int len = manifest.length();
+                this.isSystemJar = manifest.indexOf("\nImplementation-Title: Java Runtime Environment") > 0
+                        || manifest.indexOf("\nSpecification-Title: Java Platform API Specification") > 0;
+                final int classPathIdx = manifest.indexOf("\nClass-Path:");
+                if (classPathIdx >= 0) {
+                    final StringBuilder buf = new StringBuilder();
+                    int curr = classPathIdx + 12;
+                    if (curr < len && manifest.charAt(curr) == ' ') {
+                        curr++;
+                    }
+                    for (; curr < len; curr++) {
+                        final char c = manifest.charAt(curr);
+                        if (c == '\r' && (curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == '\n') {
+                            if ((curr < len - 2 ? manifest.charAt(curr + 2) : '\n') == ' ') {
+                                curr += 2;
+                            } else {
+                                break;
+                            }
+                        } else if (c == '\r') {
+                            if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
+                                curr += 1;
+                            } else {
+                                break;
+                            }
+                        } else if (c == '\n') {
+                            if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
+                                curr += 1;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            buf.append(c);
+                        }
+                    }
+                    final String classPath = buf.toString();
+                    if (!classPath.isEmpty()) {
+                        this.classPath = classPath;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Fast parser for jar manifest files. Doesn't intern strings or create Attribute objects like the Manifest
+     * class, so has lower overhead. Only extracts a few specific entries from the manifest file, if present.
+     * Assumes there is only one of each entry present in the manifest.
+     */
+    public FastManifestParser(final ZipFile jarFile, final ThreadLog log) {
+        try {
+            parseManifest(jarFile, log);
+        } catch (final IOException e) {
+            if (FastClasspathScanner.verbose) {
+                log.log("Exception while opening manifest in jarfile " + jarFile + " : " + e);
+            }
+        }
+    }
+
     /**
      * Fast parser for jar manifest files. Doesn't intern strings or create Attribute objects like the Manifest
      * class, so has lower overhead. Only extracts a few specific entries from the manifest file, if present.
@@ -22,58 +93,7 @@ public class FastManifestParser {
      */
     public FastManifestParser(final File jarFile, final ThreadLog log) {
         try (ZipFile zipFile = new ZipFile(jarFile)) {
-            final ZipEntry zipEntry = zipFile.getEntry("META-INF/MANIFEST.MF");
-            if (zipEntry != null) {
-                try (InputStream inputStream = zipFile.getInputStream(zipEntry)) {
-                    final ByteArrayOutputStream byteBuf = new ByteArrayOutputStream();
-                    byteBuf.write('\n');
-                    final byte[] data = new byte[16384];
-                    for (int bytesRead; (bytesRead = inputStream.read(data, 0, data.length)) != -1;) {
-                        byteBuf.write(data, 0, bytesRead);
-                    }
-                    byteBuf.flush();
-                    final String manifest = byteBuf.toString("UTF-8");
-                    final int len = manifest.length();
-                    this.isSystemJar = manifest.indexOf("\nImplementation-Title: Java Runtime Environment") > 0
-                            || manifest.indexOf("\nSpecification-Title: Java Platform API Specification") > 0;
-                    final int classPathIdx = manifest.indexOf("\nClass-Path:");
-                    if (classPathIdx >= 0) {
-                        final StringBuilder buf = new StringBuilder();
-                        int curr = classPathIdx + 12;
-                        if (curr < len && manifest.charAt(curr) == ' ') {
-                            curr++;
-                        }
-                        for (; curr < len; curr++) {
-                            final char c = manifest.charAt(curr);
-                            if (c == '\r' && (curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == '\n') {
-                                if ((curr < len - 2 ? manifest.charAt(curr + 2) : '\n') == ' ') {
-                                    curr += 2;
-                                } else {
-                                    break;
-                                }
-                            } else if (c == '\r') {
-                                if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
-                                    curr += 1;
-                                } else {
-                                    break;
-                                }
-                            } else if (c == '\n') {
-                                if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
-                                    curr += 1;
-                                } else {
-                                    break;
-                                }
-                            } else {
-                                buf.append(c);
-                            }
-                        }
-                        final String classPath = buf.toString();
-                        if (!classPath.isEmpty()) {
-                            this.classPath = classPath;
-                        }
-                    }
-                }
-            }
+            parseManifest(zipFile, log);
         } catch (final IOException e) {
             if (FastClasspathScanner.verbose) {
                 log.log("Exception while opening jarfile " + jarFile + " : " + e);
