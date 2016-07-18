9fd74d75c42e4281ac29b24d5aa173c58f725bf6
Luke Hutchison <luke.hutch@gmail.com>
2016-07-17 21:33:18 -0700

    Handle jars without manifests

--- src/main/java/io/github/lukehutch/fastclasspathscanner/utils/FastManifestParser.java
+++ src/main/java/io/github/lukehutch/fastclasspathscanner/utils/FastManifestParser.java
@@ -23,52 +23,54 @@ public class FastManifestParser {
     public FastManifestParser(final File jarFile, final ThreadLog log) {
         try (ZipFile zipFile = new ZipFile(jarFile)) {
             final ZipEntry zipEntry = zipFile.getEntry("META-INF/MANIFEST.MF");
-            try (InputStream inputStream = zipFile.getInputStream(zipEntry)) {
-                final ByteArrayOutputStream byteBuf = new ByteArrayOutputStream();
-                byteBuf.write('\n');
-                final byte[] data = new byte[16384];
-                for (int bytesRead; (bytesRead = inputStream.read(data, 0, data.length)) != -1;) {
-                    byteBuf.write(data, 0, bytesRead);
-                }
-                byteBuf.flush();
-                final String manifest = byteBuf.toString("UTF-8");
-                final int len = manifest.length();
-                this.isSystemJar = manifest.indexOf("\nImplementation-Title: Java Runtime Environment") > 0
-                        || manifest.indexOf("\nSpecification-Title: Java Platform API Specification") > 0;
-                final int classPathIdx = manifest.indexOf("\nClass-Path:");
-                if (classPathIdx >= 0) {
-                    final StringBuilder buf = new StringBuilder();
-                    int curr = classPathIdx + 12;
-                    if (curr < len && manifest.charAt(curr) == ' ') {
-                        curr++;
+            if (zipEntry != null) {
+                try (InputStream inputStream = zipFile.getInputStream(zipEntry)) {
+                    final ByteArrayOutputStream byteBuf = new ByteArrayOutputStream();
+                    byteBuf.write('\n');
+                    final byte[] data = new byte[16384];
+                    for (int bytesRead; (bytesRead = inputStream.read(data, 0, data.length)) != -1;) {
+                        byteBuf.write(data, 0, bytesRead);
                     }
-                    for (; curr < len; curr++) {
-                        final char c = manifest.charAt(curr);
-                        if (c == '\r' && (curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == '\n') {
-                            if ((curr < len - 2 ? manifest.charAt(curr + 2) : '\n') == ' ') {
-                                curr += 2;
-                            } else {
-                                break;
-                            }
-                        } else if (c == '\r') {
-                            if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
-                                curr += 1;
-                            } else {
-                                break;
-                            }
-                        } else if (c == '\n') {
-                            if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
-                                curr += 1;
-                            } else {
-                                break;
-                            }
-                        } else {
-                            buf.append(c);
+                    byteBuf.flush();
+                    final String manifest = byteBuf.toString("UTF-8");
+                    final int len = manifest.length();
+                    this.isSystemJar = manifest.indexOf("\nImplementation-Title: Java Runtime Environment") > 0
+                            || manifest.indexOf("\nSpecification-Title: Java Platform API Specification") > 0;
+                    final int classPathIdx = manifest.indexOf("\nClass-Path:");
+                    if (classPathIdx >= 0) {
+                        final StringBuilder buf = new StringBuilder();
+                        int curr = classPathIdx + 12;
+                        if (curr < len && manifest.charAt(curr) == ' ') {
+                            curr++;
                         }
-                    }
-                    final String classPath = buf.toString();
-                    if (!classPath.isEmpty()) {
-                        this.classPath = classPath;
+                        for (; curr < len; curr++) {
+                            final char c = manifest.charAt(curr);
+                            if (c == '\r' && (curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == '\n') {
+                                if ((curr < len - 2 ? manifest.charAt(curr + 2) : '\n') == ' ') {
+                                    curr += 2;
+                                } else {
+                                    break;
+                                }
+                            } else if (c == '\r') {
+                                if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
+                                    curr += 1;
+                                } else {
+                                    break;
+                                }
+                            } else if (c == '\n') {
+                                if ((curr < len - 1 ? manifest.charAt(curr + 1) : '\n') == ' ') {
+                                    curr += 1;
+                                } else {
+                                    break;
+                                }
+                            } else {
+                                buf.append(c);
+                            }
+                        }
+                        final String classPath = buf.toString();
+                        if (!classPath.isEmpty()) {
+                            this.classPath = classPath;
+                        }
                     }
                 }
             }
