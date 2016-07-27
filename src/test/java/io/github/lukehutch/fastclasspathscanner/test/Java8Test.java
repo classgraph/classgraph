1f1743d55d6f6cc48fae593d7ffd0778b7f8cbf7
Luke Hutchison <luke.hutch@gmail.com>
2016-07-27 14:36:38 -0700

    Test for #69

---
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/test/Java8Test.java
@@ -0,0 +1,22 @@
+package io.github.lukehutch.fastclasspathscanner.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
+
+public class Java8Test {
+    @Test
+    public void stream() throws Exception {
+        final ScanResult scanResult = new FastClasspathScanner().scan();
+        assertThat(scanResult.getClassNameToClassInfo().values().stream()
+                .filter(c -> c.getClassName().contains("ClsSub")).map(c -> c.getClassName())
+                .collect(Collectors.toSet())).containsExactly(
+                        "io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSub",
+                        "io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSubSub");
+    }
+}
