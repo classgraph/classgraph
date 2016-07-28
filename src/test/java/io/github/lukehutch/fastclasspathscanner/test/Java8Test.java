083396a041abad955977db89f674b279b014e0fb
Luke Hutchison <luke.hutch@gmail.com>
2016-07-28 01:12:11 -0700

    Add a test

--- src/test/java/io/github/lukehutch/fastclasspathscanner/test/Java8Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/test/Java8Test.java
@@ -8,6 +8,11 @@ import org.junit.Test;
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSub;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSubSub;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.Iface;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.IfaceSub;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.IfaceSubSub;
 
 public class Java8Test {
     @Test
@@ -18,7 +23,18 @@ public class Java8Test {
                         .filter(c -> c.getClassName().contains("ClsSub")) //
                         .map(c -> c.getClassName()) //
                         .collect(Collectors.toSet())) //
-                                .containsExactly("io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSub",
-                                        "io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSubSub");
+                                .containsExactly(ClsSub.class.getName(), ClsSubSub.class.getName());
+    }
+
+    @Test
+    public void streamHasSuperInterface() throws Exception {
+        final ScanResult scanResult = new FastClasspathScanner().scan();
+        assertThat( //
+                scanResult.getClassNameToClassInfo().values().stream() //
+                        .filter(c -> c.isImplementedInterface()) //
+                        .filter(c -> c.hasSuperinterface(Iface.class.getName())) //
+                        .map(c -> c.getClassName()) //
+                        .collect(Collectors.toSet())) //
+                                .containsExactly(IfaceSub.class.getName(), IfaceSubSub.class.getName());
     }
 }
