fdf0f621cb2473ce7e0c1f9bff7b8f237cda0d90
Luke Hutchison <luke.hutch@gmail.com>
2016-07-28 02:09:06 -0700

    Add test

--- src/test/java/io/github/lukehutch/fastclasspathscanner/test/Java8Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/test/Java8Test.java
@@ -1,18 +1,23 @@
 package io.github.lukehutch.fastclasspathscanner.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.assertThat;
 
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.scanner.ClassInfo;
 import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
 import io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSub;
 import io.github.lukehutch.fastclasspathscanner.test.whitelisted.ClsSubSub;
 import io.github.lukehutch.fastclasspathscanner.test.whitelisted.Iface;
 import io.github.lukehutch.fastclasspathscanner.test.whitelisted.IfaceSub;
 import io.github.lukehutch.fastclasspathscanner.test.whitelisted.IfaceSubSub;
+import io.github.lukehutch.fastclasspathscanner.test.whitelisted.Impl1;
 
 public class Java8Test {
     @Test
@@ -35,5 +40,14 @@ public class Java8Test {
                         .map(c -> c.getClassName()) //
                         .collect(Collectors.toSet())) //
                                 .containsExactly(IfaceSub.class.getName(), IfaceSubSub.class.getName());
+    }
+
+    @Test
+    public void useClassNameToClassInfo() {
+        ScanResult scanResult = new FastClasspathScanner().scan();
+        Map<String, ClassInfo> classNameToClassInfo = scanResult.getClassNameToClassInfo();
+        ClassInfo callableClassInfo = classNameToClassInfo.get(Iface.class.getName());
+        List<String> impls = callableClassInfo.getNamesOfClassesImplementing();
+        assertThat(impls.contains(Impl1.class.getName())).isTrue();
     }
 }
