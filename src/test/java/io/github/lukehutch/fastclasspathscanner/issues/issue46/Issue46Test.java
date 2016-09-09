f6ed14682d35cebb498938937d710ed4b850f0eb
Luke Hutchison <luke.hutch@gmail.com>
2016-09-09 05:55:41 -0700

    Rename test jar to zip -- Maven won't include jars during release:perform

--- src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue46/Issue46Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue46/Issue46Test.java
@@ -47,7 +47,7 @@ public class Issue46Test {
                 }
             }
         }
-        final String jarPath = Issue46Test.class.getClassLoader().getResource("nested-jars-level1.jar").getPath()
+        final String jarPath = Issue46Test.class.getClassLoader().getResource("nested-jars-level1.zip").getPath()
                 + "!level2.jar!level3.jar!classpath1/classpath2";
         assertThat(new FastClasspathScanner().overrideClasspath(jarPath).scan().getNamesOfAllClasses())
                 .containsOnly("com.test.Test");
