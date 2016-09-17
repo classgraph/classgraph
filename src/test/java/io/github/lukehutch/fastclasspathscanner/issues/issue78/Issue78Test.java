419017bf0a3f65143b333ad8275b1e390160fa7a
Luke Hutchison <luke.hutch@gmail.com>
2016-09-17 02:17:03 -0700

    Fix test

--- src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue78/Issue78Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue78/Issue78Test.java
@@ -2,10 +2,13 @@ package io.github.lukehutch.fastclasspathscanner.issues.issue78;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
+
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 
 public class Issue78Test {
-    public static void main(final String[] args) {
+    @Test
+    public void issue78() {
         assertThat(new FastClasspathScanner(Issue78Test.class.getName()).scan().getNamesOfAllStandardClasses())
                 .containsOnly(Issue78Test.class.getName());
     }
