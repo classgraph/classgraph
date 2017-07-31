d104d44315c6aa6dc5f2ae6c69d7e9745db1b8dc
Luke Hutchison <luke.hutch@gmail.com>
2017-07-31 11:30:31 -0600

    Add copyright and license info

--- src/main/java/io/github/lukehutch/fastclasspathscanner/classloaderhandler/WebsphereTraditionalClassLoaderHandler.java
+++ src/main/java/io/github/lukehutch/fastclasspathscanner/classloaderhandler/WebsphereTraditionalClassLoaderHandler.java
@@ -1,18 +1,47 @@
+/*
+ * This file is part of FastClasspathScanner.
+ * 
+ * Author: Sergey Bespalov
+ * 
+ * Hosted at: https://github.com/lukehutch/fast-classpath-scanner
+ * 
+ * --
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Sergey Bespalov
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package io.github.lukehutch.fastclasspathscanner.classloaderhandler;
 
-import io.github.lukehutch.fastclasspathscanner.classloaderhandler.ClassLoaderHandler;
 import io.github.lukehutch.fastclasspathscanner.scanner.ClasspathFinder;
 import io.github.lukehutch.fastclasspathscanner.utils.LogNode;
 import io.github.lukehutch.fastclasspathscanner.utils.ReflectionUtils;
 
 public class WebsphereTraditionalClassLoaderHandler implements ClassLoaderHandler {
 
-    public boolean handle(ClassLoader classloader, ClasspathFinder classpathFinder, LogNode log) throws Exception {
+    @Override
+    public boolean handle(final ClassLoader classloader, final ClasspathFinder classpathFinder, final LogNode log)
+            throws Exception {
         for (Class<?> c = classloader.getClass(); c != null; c = c.getSuperclass()) {
             if (!"com.ibm.ws.classloader.CompoundClassLoader".equals(c.getName())) {
                 continue;
             }
-            String classpath = (String) ReflectionUtils.invokeMethod(classloader, "getClassPath");
+            final String classpath = (String) ReflectionUtils.invokeMethod(classloader, "getClassPath");
             return classpathFinder.addClasspathElements(classpath, classloader, log);
         }
         return false;
