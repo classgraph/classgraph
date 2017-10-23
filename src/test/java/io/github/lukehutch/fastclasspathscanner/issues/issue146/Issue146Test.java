40c04d25d60f831ae7c02b7f4d4ca20de485c58d
Luke Hutchison <luke.hutch@gmail.com>
2017-10-22 23:47:56 -0600

    Test for #146

---
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue146/Issue146Test.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of FastClasspathScanner.
+ * 
+ * Author: Luke Hutchison
+ * 
+ * Hosted at: https://github.com/lukehutch/fast-classpath-scanner
+ * 
+ * --
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Luke Hutchison
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.lukehutch.fastclasspathscanner.issues.issue146;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+
+public class Issue146Test {
+    @Test
+    public void issue146Test() throws IOException {
+        // Scans io.github.lukehutch.fastclasspathscanner.issues.issue146.CompiledWithJDK8,
+        // which is in src/test/resources
+        String pkg = Issue146Test.class.getPackage().getName();
+        assertThat(new FastClasspathScanner(pkg) //
+                .enableMethodInfo() //
+                .scan() //
+                .getClassNameToClassInfo() //
+                .get(pkg + "." + "CompiledWithJDK8") //
+                .getMethodInfo("method") //
+                .get(0) //
+                .getParameterNames()) //
+                        .containsExactly("param0", "param1", "param2");
+    }
+}
---
+++ src/test/resources/io/github/lukehutch/fastclasspathscanner/issues/issue146/CompiledWithJDK8.java
@@ -0,0 +1,9 @@
+package io.github.lukehutch.fastclasspathscanner.issues.issue146;
+
+// Compile this with JDK8, using the commandline switch: -parameters
+public class CompiledWithJDK8 {
+
+    public void method(int param0, String param1, double[] param2) {
+    }
+    
+}
