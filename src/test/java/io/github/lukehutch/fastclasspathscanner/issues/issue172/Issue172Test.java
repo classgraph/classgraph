ca911a23a6a5efae7bdf33c659b6e0c675a1e7f3
Luke Hutchison <luke.hutch@gmail.com>
2018-01-24 16:16:41 -0700

    Remove temp code

--- src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue172/Issue172Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue172/Issue172Test.java
@@ -4,14 +4,11 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import io.github.lukehutch.fastclasspathscanner.utils.TypeParser;
 
 public class Issue172Test {
     @Test
     public void classAnnotationParameters() throws IOException {
         TypeParser.parseMethodSignature("<T:Ljava/lang/Object;>(Lcom/xyz/TestC<TT;>;)Lcom/xyz/TestB<TT;>;");
-
-        new FastClasspathScanner().overrideClasspath("/tmp/junkzip").verbose().scan();
     }
 }
