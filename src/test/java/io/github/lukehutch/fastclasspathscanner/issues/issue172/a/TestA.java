c1b727aa7b2b53eed497e233091b62cb752c7c21
Luke Hutchison <luke.hutch@gmail.com>
2018-01-24 07:16:41 -0700

    Fix type inference

--- src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue172/a/TestA.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue172/a/TestA.java
@@ -1,8 +1,7 @@
 package io.github.lukehutch.fastclasspathscanner.issues.issue172.a;
 
 public class TestA {
-    public <T> TestB<T> toTestB(TestC<T> other)
-    {
-        return new TestB(other.getData());
+    public <T> TestB<T> toTestB(final TestC<T> other) {
+        return new TestB<T>(other.getData());
     }
 }
