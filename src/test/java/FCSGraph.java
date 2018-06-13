4c18736e5cd013f69b6ca42291276f5c8f802444
Luke Hutchison <luke.hutch@gmail.com>
2018-06-13 08:21:06 -0600

    Add file

---
+++ src/test/java/FCSGraph.java
@@ -0,0 +1,19 @@
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+
+public class FCSGraph {
+
+    public static void main(final String[] args) {
+        System.out.println(new FastClasspathScanner( //
+                "io.github.lukehutch.fastclasspathscanner", //
+                "-io.github.lukehutch.fastclasspathscanner.issues",
+                "-io.github.lukehutch.fastclasspathscanner.test") //
+                        .enableMethodInfo() //
+                        .ignoreMethodVisibility() //
+                        .enableFieldInfo() //
+                        .ignoreFieldVisibility() //
+                        .strictWhitelist() //
+                        .scan() //
+                        .generateClassGraphDotFile(12, 8, false, false));
+    }
+
+}
