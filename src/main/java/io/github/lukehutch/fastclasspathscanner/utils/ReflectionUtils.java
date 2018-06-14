d805feb5dd04b0faebc043c3769dfb8b322a8a34
Luke Hutchison <luke.hutch@gmail.com>
2018-06-14 01:25:32 -0600

    Fix Java 9+ reflection warning

--- src/main/java/io/github/lukehutch/fastclasspathscanner/utils/ReflectionUtils.java
+++ src/main/java/io/github/lukehutch/fastclasspathscanner/utils/ReflectionUtils.java
@@ -36,22 +36,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 /** Reflection utility methods that can be used by ClassLoaderHandlers. */
-//isAccessible() is deprecated in JDK9 because the name is confusing, not because it is broken
-@SuppressWarnings("deprecation")
 public class ReflectionUtils {
-    // This unused method includes a deprecated method call, so that the @SuppressWarnings("deprecation") above
-    // does not give a warning on JDK versions before 9 (where the isAccessible() method is not yet deprecated),
-    // but can still suppress the deprecation warnings for JDK9+. 
-    @SuppressWarnings("unused")
-    private static void unusedMethod() {
-        new Thread().destroy();
-    }
-
     /**
      * Get the value of the named field in the class of the given object or any of its superclasses. If an exception
      * is thrown while trying to read the field, and throwException is true, then IllegalArgumentException is thrown
@@ -66,12 +58,8 @@ public class ReflectionUtils {
                 try {
                     final Field field = classOrSuperclass.getDeclaredField(fieldName);
                     try {
-                        if (!field.isAccessible()) {
-                            field.setAccessible(true);
-                        }
+                        field.setAccessible(true);
                     } catch (final Exception e) {
-                        // In JDK9+, may throw InaccessibleObjectException.
-                        // However, still try to invoke the method below.
                     }
                     return field.get(obj);
                 } catch (final NoSuchFieldException e) {
@@ -105,12 +93,8 @@ public class ReflectionUtils {
                 try {
                     final Field field = classOrSuperclass.getDeclaredField(fieldName);
                     try {
-                        if (!field.isAccessible()) {
-                            field.setAccessible(true);
-                        }
+                        field.setAccessible(true);
                     } catch (final Exception e) {
-                        // In JDK9+, may throw InaccessibleObjectException.
-                        // However, still try to invoke the method below.
                     }
                     return field.get(null);
                 } catch (final NoSuchFieldException e) {
@@ -130,6 +114,46 @@ public class ReflectionUtils {
         return null;
     }
 
+    // Iterate through implemented interfaces, top-down, then superclass to subclasses, top-down
+    // (since higher-up superclasses and superinterfaces have the highest chance of being visible)
+    private static List<Class<?>> getReverseMethodAttemptOrder(final Class<?> cls) {
+        final List<Class<?>> reverseAttemptOrder = new ArrayList<>();
+
+        // Iterate from class to its superclasses
+        for (Class<?> c = cls; c != null && c != Object.class; c = c.getSuperclass()) {
+            reverseAttemptOrder.add(c);
+        }
+
+        // Find interfaces and superinterfaces implemented by this class or its superclasses
+        final Set<Class<?>> addedIfaces = new HashSet<>();
+        final LinkedList<Class<?>> ifaceQueue = new LinkedList<>();
+        for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
+            if (c.isInterface()) {
+                if (addedIfaces.add(c)) {
+                    ifaceQueue.add(c);
+                }
+            }
+            for (final Class<?> iface : c.getInterfaces()) {
+                if (addedIfaces.add(iface)) {
+                    ifaceQueue.add(iface);
+                }
+            }
+        }
+        while (!ifaceQueue.isEmpty()) {
+            final Class<?> iface = ifaceQueue.remove();
+            reverseAttemptOrder.add(iface);
+            final Class<?>[] superIfaces = iface.getInterfaces();
+            if (superIfaces.length > 0) {
+                for (final Class<?> superIface : superIfaces) {
+                    if (addedIfaces.add(superIface)) {
+                        ifaceQueue.add(superIface);
+                    }
+                }
+            }
+        }
+        return reverseAttemptOrder;
+    }
+
     /**
      * Invoke the named method in the given object or its superclasses. If an exception is thrown while trying to
      * call the method, and throwException is true, then IllegalArgumentException is thrown wrapping the cause,
@@ -139,62 +163,19 @@ public class ReflectionUtils {
     public static Object invokeMethod(final Object obj, final String methodName, final boolean throwException)
             throws IllegalArgumentException, NullPointerException {
         if (obj != null) {
-            IllegalAccessException illegalAccessException = null;
-            // Try calling method in class or superclass
             final Class<? extends Object> cls = obj.getClass();
-            for (Class<?> classOrSuperclass = cls; classOrSuperclass != null; //
-                    classOrSuperclass = classOrSuperclass.getSuperclass()) {
-                try {
-                    final Method method = classOrSuperclass.getDeclaredMethod(methodName);
-                    try {
-                        if (!method.isAccessible()) {
-                            method.setAccessible(true);
-                        }
-                    } catch (final Exception e) {
-                        // In JDK9+, may throw InaccessibleObjectException.
-                        // However, still try to invoke the method below.
-                    }
-                    return method.invoke(obj);
-                } catch (final NoSuchMethodException e) {
-                    // Try parent
-                } catch (final IllegalAccessException e) {
-                    // In Java 9+, need to try parent in case of illegal access, in case one of them succeeds
-                    // (this is needed for the case where a subclass or implementation of an interface is not
-                    // visible to the caller, but the superclass is)
-                    illegalAccessException = e;
-                } catch (final Throwable e) {
-                    if (throwException) {
-                        throw new IllegalArgumentException("Could not invoke method \"" + methodName + "\"", e);
-                    }
-                }
-            }
 
-            // If that failed, find interfaces implemented by this class or its superclasses
-            final LinkedList<Class<?>> implementedIfacesQueue = new LinkedList<>();
-            final Set<Class<?>> addedIfaces = new HashSet<>();
-            for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
-                if (c.isInterface()) {
-                    if (addedIfaces.add(c)) {
-                        implementedIfacesQueue.add(c);
-                    }
-                }
-                for (final Class<?> iface : c.getInterfaces()) {
-                    if (addedIfaces.add(iface)) {
-                        implementedIfacesQueue.add(iface);
-                    }
-                }
-            }
-
-            // Iterate through implemented interfaces
-            while (!implementedIfacesQueue.isEmpty()) {
-                final Class<?> iface = implementedIfacesQueue.remove();
+            // Iterate through implemented interfaces, top-down, then superclass to subclasses, top-down
+            // (since higher-up superclasses and superinterfaces have the highest chance of being visible)
+            final List<Class<?>> reverseAttemptOrder = getReverseMethodAttemptOrder(cls);
+            IllegalAccessException illegalAccessException = null;
+            for (int i = reverseAttemptOrder.size() - 1; i >= 0; i--) {
+                final Class<?> iface = reverseAttemptOrder.get(i);
                 try {
                     // Try calling method on interface
                     final Method method = iface.getDeclaredMethod(methodName);
                     try {
-                        if (!method.isAccessible()) {
-                            method.setAccessible(true);
-                        }
+                        method.setAccessible(true);
                     } catch (final Exception e) {
                     }
                     return method.invoke(obj);
@@ -204,21 +185,11 @@ public class ReflectionUtils {
                     illegalAccessException = e;
                 } catch (final Throwable e) {
                     if (throwException) {
-                        throw new IllegalArgumentException(
-                                "Exception while invoking default method \"" + methodName + "\"", e);
-                    }
-                }
-                // Add any superinterfaces to end of queue
-                final Class<?>[] superIfaces = iface.getInterfaces();
-                if (superIfaces.length > 0) {
-                    for (final Class<?> superIface : superIfaces) {
-                        if (addedIfaces.add(superIface)) {
-                            implementedIfacesQueue.add(superIface);
-                        }
+                        throw new IllegalArgumentException("Exception while invoking method \"" + methodName + "\"",
+                                e);
                     }
                 }
             }
-
             if (throwException) {
                 if (illegalAccessException != null) {
                     throw new IllegalArgumentException("Method \"" + methodName + "\" is not accessible",
@@ -242,62 +213,19 @@ public class ReflectionUtils {
     public static Object invokeMethod(final Object obj, final String methodName, final Class<?> argType,
             final Object arg, final boolean throwException) throws IllegalArgumentException, NullPointerException {
         if (obj != null) {
-            // Try calling method in class or superclass
-            IllegalAccessException illegalAccessException = null;
             final Class<? extends Object> cls = obj.getClass();
-            for (Class<?> classOrSuperclass = cls; classOrSuperclass != null; //
-                    classOrSuperclass = classOrSuperclass.getSuperclass()) {
-                try {
-                    final Method method = classOrSuperclass.getDeclaredMethod(methodName, argType);
-                    try {
-                        if (!method.isAccessible()) {
-                            method.setAccessible(true);
-                        }
-                    } catch (final Exception e) {
-                        // In JDK9+, may throw InaccessibleObjectException.
-                        // However, still try to invoke the method below.
-                    }
-                    return method.invoke(obj, arg);
-                } catch (final NoSuchMethodException e) {
-                    // Try parent
-                } catch (final IllegalAccessException e) {
-                    // In Java 9+, need to try parent in case of illegal access, in case one of them succeeds
-                    // (this is needed for the case where a subclass or implementation of an interface is not
-                    // visible to the caller, but the superclass is)
-                    illegalAccessException = e;
-                } catch (final Throwable e) {
-                    if (throwException) {
-                        throw new IllegalArgumentException("Could not invoke method \"" + methodName + "\"", e);
-                    }
-                }
-            }
 
-            // If that failed, find interfaces implemented by this class or its superclasses
-            final LinkedList<Class<?>> implementedIfacesQueue = new LinkedList<>();
-            final Set<Class<?>> addedIfaces = new HashSet<>();
-            for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
-                if (c.isInterface()) {
-                    if (addedIfaces.add(c)) {
-                        implementedIfacesQueue.add(c);
-                    }
-                }
-                for (final Class<?> iface : c.getInterfaces()) {
-                    if (addedIfaces.add(iface)) {
-                        implementedIfacesQueue.add(iface);
-                    }
-                }
-            }
-
-            // Iterate through implemented interfaces
-            while (!implementedIfacesQueue.isEmpty()) {
-                final Class<?> iface = implementedIfacesQueue.remove();
+            // Iterate through implemented interfaces, top-down, then superclass to subclasses, top-down
+            // (since higher-up superclasses and superinterfaces have the highest chance of being visible)
+            final List<Class<?>> reverseAttemptOrder = getReverseMethodAttemptOrder(cls);
+            IllegalAccessException illegalAccessException = null;
+            for (int i = reverseAttemptOrder.size() - 1; i >= 0; i--) {
+                final Class<?> iface = reverseAttemptOrder.get(i);
                 try {
                     // Try calling method on interface
                     final Method method = iface.getDeclaredMethod(methodName, argType);
                     try {
-                        if (!method.isAccessible()) {
-                            method.setAccessible(true);
-                        }
+                        method.setAccessible(true);
                     } catch (final Exception e) {
                     }
                     return method.invoke(obj, arg);
@@ -307,21 +235,11 @@ public class ReflectionUtils {
                     illegalAccessException = e;
                 } catch (final Throwable e) {
                     if (throwException) {
-                        throw new IllegalArgumentException(
-                                "Exception while invoking default method \"" + methodName + "\"", e);
-                    }
-                }
-                // Add any superinterfaces to end of queue
-                final Class<?>[] superIfaces = iface.getInterfaces();
-                if (superIfaces.length > 0) {
-                    for (final Class<?> superIface : superIfaces) {
-                        if (addedIfaces.add(superIface)) {
-                            implementedIfacesQueue.add(superIface);
-                        }
+                        throw new IllegalArgumentException("Exception while invoking method \"" + methodName + "\"",
+                                e);
                     }
                 }
             }
-
             if (throwException) {
                 if (illegalAccessException != null) {
                     throw new IllegalArgumentException("Method \"" + methodName + "\" is not accessible",
@@ -347,12 +265,8 @@ public class ReflectionUtils {
             try {
                 final Method method = cls.getDeclaredMethod(methodName);
                 try {
-                    if (!method.isAccessible()) {
-                        method.setAccessible(true);
-                    }
+                    method.setAccessible(true);
                 } catch (final Exception e) {
-                    // In JDK9+, may throw InaccessibleObjectException.
-                    // However, still try to invoke the method below.
                 }
                 return method.invoke(null);
             } catch (final Throwable e) {
@@ -377,12 +291,8 @@ public class ReflectionUtils {
             try {
                 final Method method = cls.getDeclaredMethod(methodName, argType);
                 try {
-                    if (!method.isAccessible()) {
-                        method.setAccessible(true);
-                    }
+                    method.setAccessible(true);
                 } catch (final Exception e) {
-                    // In JDK9+, may throw InaccessibleObjectException.
-                    // However, still try to invoke the method below.
                 }
                 return method.invoke(null, arg);
             } catch (final Throwable e) {
@@ -451,43 +361,23 @@ public class ReflectionUtils {
                 }
             }
 
-            // Find interfaces implemented by this class or its superclasses
-            final LinkedList<Class<?>> implementedIfacesQueue = new LinkedList<>();
-            final Set<Class<?>> addedIfaces = new HashSet<>();
-            for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
-                if (c.isInterface()) {
-                    if (addedIfaces.add(c)) {
-                        implementedIfacesQueue.add(c);
-                    }
-                }
-                for (final Class<?> iface : c.getInterfaces()) {
-                    if (addedIfaces.add(iface)) {
-                        implementedIfacesQueue.add(iface);
-                    }
-                }
-            }
-
-            // Iterate through implemented interfaces
-            while (!implementedIfacesQueue.isEmpty()) {
-                final Class<?> iface = implementedIfacesQueue.remove();
+            // Iterate through implemented interfaces, top-down, then superclass to subclasses, top-down
+            // (since higher-up superclasses and superinterfaces have the highest chance of being visible)
+            final List<Class<?>> reverseAttemptOrder = getReverseMethodAttemptOrder(cls);
+            IllegalAccessException illegalAccessException = null;
+            for (int i = reverseAttemptOrder.size() - 1; i >= 0; i--) {
+                final Class<?> iface = reverseAttemptOrder.get(i);
                 try {
                     // Try calling default method on interface
                     return tryInvokeDefaultMethod(iface, methodName, returnType, classLoader, throwException);
                 } catch (final NoSuchMethodException e) {
                     // Fall through if method not found, since a superinterface might have the method 
+                } catch (final IllegalAccessException e) {
+                    illegalAccessException = e;
                 } catch (final Throwable e) {
                     if (throwException) {
-                        throw new IllegalArgumentException(
-                                "Exception while invoking default method \"" + methodName + "\"", e);
-                    }
-                }
-                // Add any superinterfaces to end of queue
-                final Class<?>[] superIfaces = iface.getInterfaces();
-                if (superIfaces.length > 0) {
-                    for (final Class<?> superIface : superIfaces) {
-                        if (addedIfaces.add(superIface)) {
-                            implementedIfacesQueue.add(superIface);
-                        }
+                        throw new IllegalArgumentException("Exception while invoking method \"" + methodName + "\"",
+                                e);
                     }
                 }
             }
