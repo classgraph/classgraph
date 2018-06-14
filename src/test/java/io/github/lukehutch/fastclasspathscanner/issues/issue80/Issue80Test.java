44e15e87b93c55867089df87039eb1f7454f2285
Luke Hutchison <luke.hutch@gmail.com>
2018-06-14 00:42:33 -0600

    Test now works out of the box on JDK9+

--- src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue80/Issue80Test.java
+++ src/test/java/io/github/lukehutch/fastclasspathscanner/issues/issue80/Issue80Test.java
@@ -5,17 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
-import io.github.lukehutch.fastclasspathscanner.utils.ReflectionUtils;
 
 public class Issue80Test {
     @Test
     public void issue80() {
-        // TODO: this test will fail in JDK9+, because there is no rt.jar.
-        // TODO: Need to scan system modules when "!!" is in the scan spec.
-        if (!ReflectionUtils.JAVA_VERSION_IS_9_PLUS) {
-            assertThat(new FastClasspathScanner("!!", "java.util").scan().getNamesOfAllStandardClasses())
-                    .contains("java.util.ArrayList");
-        }
-        assertThat(new FastClasspathScanner("java.util").scan().getNamesOfAllStandardClasses()).isEmpty();
+        assertThat(new FastClasspathScanner("!!", "java.util").scan().getNamesOfAllStandardClasses())
+                .contains("java.util.ArrayList");
     }
 }
