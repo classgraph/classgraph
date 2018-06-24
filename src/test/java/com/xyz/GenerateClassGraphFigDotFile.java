90df8e6236351ea21a12755d1cfa1d1514736964
Luke Hutchison <luke.hutch@gmail.com>
2018-06-24 05:03:32 -0600

    Add JSON example

--- src/test/java/com/xyz/GenerateClassGraphFigDotFile.java
+++ src/test/java/com/xyz/GenerateClassGraphFigDotFile.java
@@ -1,16 +1,17 @@
 package com.xyz;
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
 
 public class GenerateClassGraphFigDotFile {
     public static void main(final String[] args) {
-
-        System.out.println(new FastClasspathScanner("com.xyz.fig") //
+        ScanResult scanResult = new FastClasspathScanner("com.xyz.fig") //
                 .ignoreFieldVisibility() //
                 .enableFieldInfo() //
                 .ignoreMethodVisibility() //
                 .enableMethodInfo() //
-                .scan() //
-                .generateClassGraphDotFile(9.2f, 8.0f));
+                .scan();
+        System.out.println(scanResult.generateClassGraphDotFile(9.2f, 8.0f));
+        // System.out.println(scanResult.toJSON(2));
     }
 }
