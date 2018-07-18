d7964cf82b33ca53e48e73f0758a677abecf437c
Luke Hutchison <luke.hutch@gmail.com>
2018-07-17 21:32:09 -0600

    Switch from Moditect moduleInfo to moduleInfoFile for JDK8 compat (#211)

---
+++ src/main/resources/module-info.java
@@ -0,0 +1,8 @@
+module io.github.lukehutch.fastclasspathscanner {
+    exports io.github.lukehutch.fastclasspathscanner;
+    exports io.github.lukehutch.fastclasspathscanner.classloaderhandler;
+    exports io.github.lukehutch.fastclasspathscanner.matchprocessor;
+    exports io.github.lukehutch.fastclasspathscanner.scanner;
+    exports io.github.lukehutch.fastclasspathscanner.typesignature;
+    exports io.github.lukehutch.fastclasspathscanner.utils;
+}
