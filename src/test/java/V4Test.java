92c91008ce0a84e2e75c7ece15b647d7b543c907
Luke Hutchison <luke.hutch@gmail.com>
2018-07-25 22:05:19 -0600

    Remove old code

--- src/test/java/V4Test.java
+++ src/test/java/V4Test.java
@@ -5,23 +5,10 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 public class V4Test {
 
     public static void main(final String[] args) throws IOException {
-        //        new FastClasspathScanner() //
-        //                // .verbose() //
-        //                .unBlacklistSystemPackages() //
-        //                .enableClassInfo() //
-        //                .scan() //
-        //                .getResourcesWithExtension("png") // 
-        //                .forEachByteArrayThenClose((res, arr) -> {
-        //                    final String string = new String(arr);
-        //                    System.out.println("======================================");
-        //                    System.out.println(res.getPathRelativeToPackageRoot());
-        //                    System.out.println(string.substring(0, Math.min(120, string.length())));
-        //                });
-
         final long t0 = System.nanoTime();
         new FastClasspathScanner() //
                 // .verbose() //
-                .enableSystemPackages() //
+                // .enableSystemPackages() //
                 .enableClassInfo()
                 //.enableMethodInfo().enableFieldInfo() //
                 .ignoreClassVisibility() //
