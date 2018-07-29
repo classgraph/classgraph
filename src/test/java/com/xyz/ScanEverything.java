893b9767bc98470f2965847d6948c1c64bbcbd91
Luke Hutchison <luke.hutch@gmail.com>
2018-07-29 13:58:04 -0600

    Rename class

---
+++ src/test/java/com/xyz/ScanEverything.java
@@ -0,0 +1,22 @@
+package com.xyz;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import io.github.classgraph.ClassGraph;
+
+public class ScanEverything {
+
+    @Test
+    public void v4Test() throws IOException {
+        // final long t0 = System.nanoTime();
+        //        new ClassGraph() //
+        //                // .verbose() //
+        //                .enableAllInfo() //
+        //                .enableSystemPackages() //
+        //                .scan();
+        // System.out.println((System.nanoTime() - t0) * 1.0e-9);
+    }
+
+}
