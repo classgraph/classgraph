5d5a7e2666dc524df08bfb075288fd24d45b413f
Luke Hutchison <luke.hutch@gmail.com>
2018-07-29 19:15:10 -0600

    Updates

--- src/test/java/com/xyz/ScanEverything.java
+++ src/test/java/com/xyz/ScanEverything.java
@@ -1,22 +1,20 @@
 package com.xyz;
 
-import java.io.IOException;
-
-import org.junit.Test;
-
-import io.github.classgraph.ClassGraph;
-
 public class ScanEverything {
 
-    @Test
-    public void v4Test() throws IOException {
-        // final long t0 = System.nanoTime();
-        //        new ClassGraph() //
-        //                // .verbose() //
-        //                .enableAllInfo() //
-        //                .enableSystemPackages() //
-        //                .scan();
-        // System.out.println((System.nanoTime() - t0) * 1.0e-9);
-    }
+    //    @Test
+    //    public void scanEverything() throws IOException {
+    //        final long t0 = System.nanoTime();
+    //        new ClassGraph() //
+    //                // .verbose() //
+    //                // .whitelistPackages("io.github") //
+    //                .enableAllInfo() //
+    //                // .enableSystemPackages() //
+    //                .scan() //
+    //                .getAllClasses() //
+    //        // .loadClasses() //
+    //        ;
+    //        System.out.println((System.nanoTime() - t0) * 1.0e-9);
+    //    }
 
 }
