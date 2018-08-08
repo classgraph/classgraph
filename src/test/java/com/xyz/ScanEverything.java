a9b60affae11c6bae99a18d4442df91940b0f839
Luke Hutchison <luke.hutch@gmail.com>
2018-08-07 21:18:16 -0600

    Comment out code

--- src/test/java/com/xyz/ScanEverything.java
+++ src/test/java/com/xyz/ScanEverything.java
@@ -9,32 +9,32 @@ import io.github.classgraph.ScanResult;
 
 public class ScanEverything {
 
-    @Test
-    public void scanEverything() throws IOException {
-        final long t0 = System.nanoTime();
-        final ScanResult scanResult = new ClassGraph() //
-                // .verbose() //
-                // .whitelistPackages("io.github") //
-                // .enableAllInfo() //
-                .enableSystemPackages() //
-                .disableJarScanning() //
-                .disableModuleScanning() //
-                .scan() //
-        //.getAllClasses() //
-        // .loadClasses() //
-        ;
-        final long t1 = System.nanoTime();
-        System.out.println((t1 - t0) * 1.0e-9);
-
-        System.out.println(scanResult.getAllResources().size());
-
-        for (int i = 0; i < 500; i++) {
-            scanResult.getAllResources().forEachByteArray((r, b) -> {
-            });
-        }
-
-        final long t2 = System.nanoTime();
-        System.out.println((t2 - t1) * 1.0e-9);
-    }
+//    @Test
+//    public void scanEverything() throws IOException {
+//        final long t0 = System.nanoTime();
+//        final ScanResult scanResult = new ClassGraph() //
+//                // .verbose() //
+//                // .whitelistPackages("io.github") //
+//                // .enableAllInfo() //
+//                .enableSystemPackages() //
+//                .disableJarScanning() //
+//                .disableModuleScanning() //
+//                .scan() //
+//        //.getAllClasses() //
+//        // .loadClasses() //
+//        ;
+//        final long t1 = System.nanoTime();
+//        System.out.println((t1 - t0) * 1.0e-9);
+//
+//        System.out.println(scanResult.getAllResources().size());
+//
+//        for (int i = 0; i < 500; i++) {
+//            scanResult.getAllResources().forEachByteArray((r, b) -> {
+//            });
+//        }
+//
+//        final long t2 = System.nanoTime();
+//        System.out.println((t2 - t1) * 1.0e-9);
+//    }
 
 }
