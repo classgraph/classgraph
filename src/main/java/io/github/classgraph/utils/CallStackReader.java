b05921aa2f760c1c561652f89778f0cb75478e66
Luke Hutchison <luke.hutch@gmail.com>
2018-10-08 07:56:51 -0600

    Always use doPrivileged

--- src/main/java/io/github/classgraph/utils/CallStackReader.java
+++ src/main/java/io/github/classgraph/utils/CallStackReader.java
@@ -40,45 +40,38 @@ import java.util.List;
 class CallStackReader {
 
     /** Get the call stack via the StackWalker API (JRE 9+). */
-    private static Class<?>[] getCallStackViaStackWalker(final LogNode log) {
-        try {
-            //    // Implement the following via reflection, for JDK7 compatibility:
-            //    List<Class<?>> stackFrameClasses = new ArrayList<>();
-            //    StackWalker.getInstance(Option.RETAIN_CLASS_REFERENCE)
-            //            .forEach(sf -> stackFrameClasses.add(sf.getDeclaringClass()));
+    private static Class<?>[] getCallStackViaStackWalker(final LogNode log) throws Exception {
+        //    // Implement the following via reflection, for JDK7 compatibility:
+        //    List<Class<?>> stackFrameClasses = new ArrayList<>();
+        //    StackWalker.getInstance(Option.RETAIN_CLASS_REFERENCE)
+        //            .forEach(sf -> stackFrameClasses.add(sf.getDeclaringClass()));
 
-            final Class<?> consumerClass = Class.forName("java.util.function.Consumer");
-            final List<Class<?>> stackFrameClasses = new ArrayList<>();
-            final Class<?> stackWalkerOptionClass = Class.forName("java.lang.StackWalker$Option");
-            final Object RETAIN_CLASS_REFERENCE = Class.forName("java.lang.Enum")
-                    .getMethod("valueOf", Class.class, String.class)
-                    .invoke(null, stackWalkerOptionClass, "RETAIN_CLASS_REFERENCE");
-            final Class<?> stackWalkerClass = Class.forName("java.lang.StackWalker");
-            final Object stackWalkerInstance = stackWalkerClass.getMethod("getInstance", stackWalkerOptionClass)
-                    .invoke(null, RETAIN_CLASS_REFERENCE);
-            final Method stackFrameGetDeclaringClassMethod = Class.forName("java.lang.StackWalker$StackFrame")
-                    .getMethod("getDeclaringClass");
-            stackWalkerClass.getMethod("forEach", consumerClass).invoke(stackWalkerInstance, //
-                    // InvocationHandler proxy for Consumer<StackFrame>
-                    Proxy.newProxyInstance(consumerClass.getClassLoader(), new Class[] { consumerClass },
-                            new InvocationHandler() {
-                                @Override
-                                public Object invoke(final Object proxy, final Method method, final Object[] args)
-                                        throws Throwable {
-                                    // Consumer<StackFrame> has only one method: void accept(StackFrame)
-                                    final Class<?> declaringClass = (Class<?>) stackFrameGetDeclaringClassMethod
-                                            .invoke(args[0]);
-                                    stackFrameClasses.add(declaringClass);
-                                    return null;
-                                }
-                            }));
-            return stackFrameClasses.toArray(new Class<?>[0]);
-        } catch (final Throwable e) {
-            if (log != null) {
-                log.log("Exception while trying to obtain call stack via StackWalker", e);
-            }
-        }
-        return null;
+        final Class<?> consumerClass = Class.forName("java.util.function.Consumer");
+        final List<Class<?>> stackFrameClasses = new ArrayList<>();
+        final Class<?> stackWalkerOptionClass = Class.forName("java.lang.StackWalker$Option");
+        final Object RETAIN_CLASS_REFERENCE = Class.forName("java.lang.Enum")
+                .getMethod("valueOf", Class.class, String.class)
+                .invoke(null, stackWalkerOptionClass, "RETAIN_CLASS_REFERENCE");
+        final Class<?> stackWalkerClass = Class.forName("java.lang.StackWalker");
+        final Object stackWalkerInstance = stackWalkerClass.getMethod("getInstance", stackWalkerOptionClass)
+                .invoke(null, RETAIN_CLASS_REFERENCE);
+        final Method stackFrameGetDeclaringClassMethod = Class.forName("java.lang.StackWalker$StackFrame")
+                .getMethod("getDeclaringClass");
+        stackWalkerClass.getMethod("forEach", consumerClass).invoke(stackWalkerInstance, //
+                // InvocationHandler proxy for Consumer<StackFrame>
+                Proxy.newProxyInstance(consumerClass.getClassLoader(), new Class[] { consumerClass },
+                        new InvocationHandler() {
+                            @Override
+                            public Object invoke(final Object proxy, final Method method, final Object[] args)
+                                    throws Throwable {
+                                // Consumer<StackFrame> has only one method: void accept(StackFrame)
+                                final Class<?> declaringClass = (Class<?>) stackFrameGetDeclaringClassMethod
+                                        .invoke(args[0]);
+                                stackFrameClasses.add(declaringClass);
+                                return null;
+                            }
+                        }));
+        return stackFrameClasses.toArray(new Class<?>[0]);
     }
 
     // -------------------------------------------------------------------------------------------------------------
@@ -114,51 +107,37 @@ class CallStackReader {
     // -------------------------------------------------------------------------------------------------------------
 
     /**
-     * Find all classes on the call stack.
-     * 
-     * @throws IllegalArgumentException
-     *             if the call stack could not be read.
-     * @return the classes on the call stack.
+     * @return The classes in the call stack.
      */
     static Class<?>[] getClassContext(final LogNode log) {
-        Class<?>[] callStack;
-
         // For JRE 9+, use StackWalker to get call stack
-
         if (VersionFinder.JAVA_MAJOR_VERSION >= 9) {
-            // Try with doPrivileged()
-            callStack = AccessController.doPrivileged(new PrivilegedAction<Class<?>[]>() {
-                @Override
-                public Class<?>[] run() {
-                    return getCallStackViaStackWalker(log);
-                }
-            });
-            if (callStack != null) {
-                return callStack;
-            }
-            // Try without doPrivileged()
-            callStack = getCallStackViaStackWalker(log);
-            if (callStack != null) {
-                return callStack;
+            try {
+                // Invoke with doPrivileged -- see:
+                // http://mail.openjdk.java.net/pipermail/jigsaw-dev/2018-October/013974.html
+                return AccessController.doPrivileged(new PrivilegedAction<Class<?>[]>() {
+                    @Override
+                    public Class<?>[] run() {
+                        try {
+                            return getCallStackViaStackWalker(log);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            } catch (Throwable ignored) {
             }
         }
 
         // For JRE 7 and 8, use SecurityManager to get call stack
-
-        // Try with doPrivileged()
-        callStack = AccessController.doPrivileged(new PrivilegedAction<Class<?>[]>() {
-            @Override
-            public Class<?>[] run() {
-                return getCallStackViaSecurityManager(log);
-            }
-        });
-        if (callStack != null) {
-            return callStack;
-        }
-        // Try without doPrivileged()
-        callStack = getCallStackViaSecurityManager(log);
-        if (callStack != null) {
-            return callStack;
+        try {
+            return AccessController.doPrivileged(new PrivilegedAction<Class<?>[]>() {
+                @Override
+                public Class<?>[] run() {
+                    return getCallStackViaSecurityManager(log);
+                }
+            });
+        } catch (Throwable ignored) {
         }
 
         // As a fallback, use getStackTrace() to try to get the call stack
@@ -169,8 +148,7 @@ class CallStackReader {
             for (final StackTraceElement elt : e.getStackTrace()) {
                 try {
                     classes.add(Class.forName(elt.getClassName()));
-                } catch (final Throwable e2) {
-                    // Ignore
+                } catch (final Throwable ignored) {
                 }
             }
             if (classes.size() > 0) {
