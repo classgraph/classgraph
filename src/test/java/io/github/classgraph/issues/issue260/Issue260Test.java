2394a3dcbddf5b67a1fa780c8bb7cf9a90932072
Luke Hutchison <luke.hutch@gmail.com>
2018-10-18 01:01:45 -0600

    Remove .verbose()

--- src/test/java/io/github/classgraph/issues/issue260/Issue260Test.java
+++ src/test/java/io/github/classgraph/issues/issue260/Issue260Test.java
@@ -39,7 +39,7 @@ public class Issue260Test {
     @Test
     public void issue260Test() {
         try (ScanResult scanResult = new ClassGraph().whitelistPackages(Issue260Test.class.getPackage().getName())
-                .enableAllInfo().verbose().scan()) {
+                .enableAllInfo().scan()) {
             // Should be no exception here
             assertThat(true).isTrue();
         }
