896f0f161c6adb4c1230432af3d8e1e8b4b46ffc
diffblue-assistant <contributions@diffblue.com>
2018-10-18 13:37:30 +0100

    Add Diffblue tests

---
+++ src/test/java/io/github/classgraph/utils/WhiteBlackListTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright Diffblue Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.classgraph.utils;
+
+import io.github.classgraph.utils.WhiteBlackList;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class WhiteBlackListTest {
+
+  @Rule public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void normalizePackageOrClassNameInputNotNullOutputNotNull2() {
+
+    // Arrange
+    final String packageOrClassName = "-.,,,,-,/.";
+
+    // Act
+    final String retval = WhiteBlackList.normalizePackageOrClassName(packageOrClassName);
+
+    // Assert result
+    Assert.assertEquals("-.,,,,-,/", retval);
+  }
+
+  @Test
+  public void normalizePackageOrClassNameInputNotNullOutputNotNull3() {
+
+    // Arrange
+    final String packageOrClassName = ".-,-,--(.(";
+
+    // Act
+    final String retval = WhiteBlackList.normalizePackageOrClassName(packageOrClassName);
+
+    // Assert result
+    Assert.assertEquals("-,-,--(.(", retval);
+  }
+
+  @Test
+  public void normalizePackageOrClassNameInputNotNullOutputNotNull() {
+
+    // Arrange
+    final String packageOrClassName = ",.--,--,.-";
+
+    // Act
+    final String retval = WhiteBlackList.normalizePackageOrClassName(packageOrClassName);
+
+    // Assert result
+    Assert.assertEquals(",.--,--,.-", retval);
+  }
+
+  @Test
+  public void normalizePathInputNotNullOutputNotNull2() {
+
+    // Arrange
+    final String path = "/o";
+
+    // Act
+    final String retval = WhiteBlackList.normalizePath(path);
+
+    // Assert result
+    Assert.assertEquals("o/", retval);
+  }
+
+  @Test
+  public void classNameToClassfilePathInputNotNullOutputNotNull() {
+
+    // Arrange
+    final String className = ",/=?==/";
+
+    // Act
+    final String retval = WhiteBlackList.classNameToClassfilePath(className);
+
+    // Assert result
+    Assert.assertEquals(",/=?==/.class", retval);
+  }
+
+  @Test
+  public void normalizePathInputNotNullOutputNotNull() {
+
+    // Arrange
+    final String path = "";
+
+    // Act
+    final String retval = WhiteBlackList.normalizePath(path);
+
+    // Assert result
+    Assert.assertEquals("/", retval);
+  }
+
+  @Test
+  public void pathToPackageNameInputNotNullOutputNotNull() {
+
+    // Arrange
+    final String path = "/";
+
+    // Act
+    final String retval = WhiteBlackList.pathToPackageName(path);
+
+    // Assert result
+    Assert.assertEquals(".", retval);
+  }
+
+  @Test
+  public void packageNameToPathInputNotNullOutputNotNull() {
+
+    // Arrange
+    final String packageName = ";..Mm.";
+
+    // Act
+    final String retval = WhiteBlackList.packageNameToPath(packageName);
+
+    // Assert result
+    Assert.assertEquals(";//Mm/", retval);
+  }
+}
