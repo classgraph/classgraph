605af29c2e2965a1a8ffe780aa85938b90e54ac4
Luke Hutchison <luke.hutch@gmail.com>
2019-02-07 03:29:20 -0700

    Update copyright year

--- src/main/java/nonapi/io/github/classgraph/classloaderhandler/AntClassLoaderHandler.java
+++ src/main/java/nonapi/io/github/classgraph/classloaderhandler/AntClassLoaderHandler.java
@@ -9,7 +9,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 @jacobg, Luke Hutchison
+ * Copyright (c) 2019 @jacobg, Luke Hutchison
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without
