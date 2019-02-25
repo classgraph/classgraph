ddb64827d316a112ac9162d7a899e5f1759ffa14
Luke Hutchison <luke.hutch@gmail.com>
2019-02-25 02:58:56 -0700

    Remove unnecessary test

--- src/test/java/io/github/test/classgraph/issues/issue321/FooImpl.java
+++
@@ -1,6 +0,0 @@
-package io.github.test.classgraph.issues.issue321;
-
-public class FooImpl implements IFoo
-{
-
-}
--- src/test/java/io/github/test/classgraph/issues/issue321/IFoo.java
+++
@@ -1,6 +0,0 @@
-package io.github.test.classgraph.issues.issue321;
-
-public interface IFoo
-{
-
-}
