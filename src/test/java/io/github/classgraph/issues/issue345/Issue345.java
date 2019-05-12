1076d6adfe5da62e0e13556d46584c3b582c46fd
Luke Hutchison <luke.hutch@gmail.com>
2019-05-12 05:08:28 -0600

    Improve unit tests (#345)

--- src/test/java/io/github/classgraph/issues/issue345/Issue345.java
+++ src/test/java/io/github/classgraph/issues/issue345/Issue345.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.AbstractQueue;
 
 import org.junit.Test;
 
 import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.Resource;
 import io.github.classgraph.ScanResult;
 
 /**
@@ -16,16 +17,107 @@ import io.github.classgraph.ScanResult;
  */
 public class Issue345 {
     /**
-     * Issue 345.
+     * Superclass.
+     */
+    private static class Super {
+    }
+
+    /**
+     * Subclass.
+     */
+    public static class Sub extends Super {
+    }
+
+    /**
+     * Test that private superclasses have their {@link Resource} reference set with .ignoreClassVisibility().
      */
     @Test
-    public void issue345() {
-        try (ScanResult scanResult = new ClassGraph().enableClassInfo().enableSystemJarsAndModules()
-                .overrideClassLoaders(new URLClassLoader(
-                        new URL[] { Issue345.class.getResource("/java/util/ArrayBlockingQueue.class"),
-                                Issue345.class.getResource("/java/util/AbstractQueue.class"), }))
+    public void withIgnoreClassVisibility() {
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Super.class.getName(), Sub.class.getName())
+                .ignoreClassVisibility().scan()) {
+            final ClassInfo subClassInfo = scanResult.getClassInfo(Sub.class.getName());
+            assertThat(subClassInfo).isNotNull();
+            assertThat(subClassInfo.getResource()).isNotNull();
+            final ClassInfo superClassInfo = scanResult.getClassInfo(Super.class.getName());
+            assertThat(superClassInfo).isNotNull();
+            assertThat(superClassInfo.getResource()).isNotNull();
+        }
+    }
+
+    /**
+     * Test that private superclasses do not have their {@link Resource} reference set without
+     * .ignoreClassVisibility().
+     */
+    @Test
+    public void withoutIgnoreClassVisibility() {
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Super.class.getName(), Sub.class.getName())
                 .scan()) {
-            assertThat(scanResult.getClassInfo(AbstractQueue.class.getName()).getResource()).isNotNull();
+            final ClassInfo subClassInfo = scanResult.getClassInfo(Sub.class.getName());
+            assertThat(subClassInfo).isNotNull();
+            assertThat(subClassInfo.getResource()).isNotNull();
+            final ClassInfo superClassInfo = scanResult.getClassInfo(Super.class.getName());
+            assertThat(superClassInfo).isNotNull();
+            assertThat(superClassInfo.getResource()).isNull();
+        }
+    }
+
+    /**
+     * Test that extending scanning to superclasses causes the {@link Resource} reference to be set.
+     */
+    @Test
+    public void testExtensionToParent() {
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Sub.class.getName()).ignoreClassVisibility()
+                .scan()) {
+            final ClassInfo superClassInfo = scanResult.getClassInfo(Super.class.getName());
+            assertThat(superClassInfo).isNotNull();
+            assertThat(superClassInfo.getResource()).isNotNull();
+        }
+    }
+
+    /**
+     * Test that extending scanning to outer class causes the {@link Resource} reference to be set.
+     */
+    @Test
+    public void testExtensionToOuterClass() {
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Super.class.getName())
+                .ignoreClassVisibility().scan()) {
+            final ClassInfo outerClassInfo = scanResult.getClassInfo(Issue345.class.getName());
+            assertThat(outerClassInfo).isNotNull();
+            assertThat(outerClassInfo.getResource()).isNotNull();
+        }
+    }
+
+    /**
+     * Test that scanning is not extended to inner class, because the {@link Resource} reference is not set.
+     */
+    @Test
+    public void testNonExtensionToInnerClass() {
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Issue345.class.getName())
+                .ignoreClassVisibility().scan()) {
+            final ClassInfo innerClassInfo = scanResult.getClassInfo(Super.class.getName());
+            assertThat(innerClassInfo).isNotNull();
+            assertThat(innerClassInfo.getResource()).isNull();
+        }
+    }
+
+    /**
+     * Test that overriding classloaders does not allow other classloaders to be scanned.
+     */
+    @Test
+    public void issue345b() throws Exception {
+        // Find URL of this class' classpath element
+        URL classpathURL;
+        try (ScanResult scanResult = new ClassGraph().whitelistClasses(Issue345.class.getName()).scan()) {
+            classpathURL = scanResult.getClassInfo(Issue345.class.getName()).getClasspathElementURL();
+        }
+        // Use this to create an override URLClassLoader
+        try (ScanResult scanResult = new ClassGraph().enableClassInfo()
+                .overrideClassLoaders(new URLClassLoader(new URL[] { classpathURL })).ignoreParentClassLoaders()
+                .verbose().scan()) {
+            // Assert that this class is found in its own classloader
+            assertThat(scanResult.getClassInfo(Issue345.class.getName())).isNotNull();
+            // But that other classpath elements on the classpath are not found
+            assertThat(scanResult.getClassInfo(Test.class.getName())).isNull();
         }
     }
 }
