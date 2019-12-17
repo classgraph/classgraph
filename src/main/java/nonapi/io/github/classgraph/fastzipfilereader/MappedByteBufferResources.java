c81f6d9d37f3d11e1cff2ee370fbf5e4d85d5ca4
Luke Hutchison <luke.hutch@gmail.com>
2019-12-17 03:10:57 -0700

    Source > Cleanup

--- src/main/java/nonapi/io/github/classgraph/fastzipfilereader/MappedByteBufferResources.java
+++ src/main/java/nonapi/io/github/classgraph/fastzipfilereader/MappedByteBufferResources.java
@@ -252,7 +252,7 @@ public class MappedByteBufferResources {
                         System.runFinalization();
                         // Then try calling map again
                         byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, pos, chunkSize);
-                    } catch (IOException e2) {
+                    } catch (final IOException e2) {
                         MappedByteBufferResources.this.close(log);
                         throw e2;
                     } catch (final OutOfMemoryError | IllegalArgumentException e2) {
