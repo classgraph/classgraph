8d4518cc883420a16f0326be55e9be2e29f654be
Luke Hutchison <luke.hutch@gmail.com>
2020-02-10 01:47:45 -0700

    Source cleanup

--- src/test/perf/io/github/classgraph/issues/issue400/Issue400.java
+++ src/test/perf/io/github/classgraph/issues/issue400/Issue400.java
@@ -1,58 +1,44 @@
 package io.github.classgraph.issues.issue400;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Offset.offset;
+
 import java.net.URL;
 import java.net.URLClassLoader;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.data.Offset.offset;
-
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
 
 /**
- * Verify that a large number of stored/deflated nested JAR entries don't cause problems.
+ * Verify that a large number of stored/deflated nested JAR entries don't cause memory problems.
+ * 
+ * @author Róbert Papp ( https://github.com/TWiStErRob )
  */
 public class Issue400 {
 
     private static final long MB = 1024 * 1024;
     private static final long MEMORY_TOLERANCE = 2 * MB;
 
-    @Test
-    public void loadsStoredJarWithManyNestedEntriesAndDoesNotUseMuchMemory() {
-        loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(
-                Issue400.class.getClassLoader().getResource("issue400-nested-stored.jar"));
-    }
-
-    @Test
-    public void loadsDeflatedJarWithManyNestedEntriesAndDoesNotUseMuchMemory() {
-        loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(
-                Issue400.class.getClassLoader().getResource("issue400-nested-deflated.jar"));
-    }
-
-    private void loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(URL... jars) {
-        long ramAtStart = usedRam();
+    @SuppressWarnings("null")
+    private void loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(final URL... jars) {
+        final long ramAtStart = usedRam();
         long ramAfterScan;
-        try (
-                ScanResult scanResult = new ClassGraph()
-                        .overrideClassLoaders(new URLClassLoader(jars))
-                        .enableAllInfo()
-                        .scan()
-        ) {
+        try (ScanResult scanResult = new ClassGraph().overrideClassLoaders(new URLClassLoader(jars)).enableAllInfo()
+                .scan()) {
             ramAfterScan = usedRam();
             // There are no classes in any of the JARs.
             assertThat(scanResult.getAllClassesAsMap()).isEmpty();
             // Check if it contains the JAR and all nested entries.
             assertThat(scanResult.getClasspathURLs()).hasSize(1 + 128);
         }
-        long ramAtEnd = usedRam();
+        final long ramAtEnd = usedRam();
 
         assertThat(ramAfterScan)
                 .withFailMessage("Memory usage while using ScanResult should stay within reasonable range: "
                         + "went from %s to %s MB.", ramAtStart / MB, ramAfterScan / MB)
-                .isGreaterThanOrEqualTo(ramAtStart)
-                .isCloseTo(ramAtStart, offset(MEMORY_TOLERANCE));
+                .isGreaterThanOrEqualTo(ramAtStart).isCloseTo(ramAtStart, offset(MEMORY_TOLERANCE));
 
         assertThat(ramAtStart)
                 .withFailMessage("Memory usage after cleaning up should stay within reasonable range: "
@@ -61,11 +47,30 @@ public class Issue400 {
     }
 
     /**
+     * Test jar with stored entries.
+     */
+    @Test
+    public void loadsStoredJarWithManyNestedEntriesAndDoesNotUseMuchMemory() {
+        loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(
+                Issue400.class.getClassLoader().getResource("issue400-nested-stored.jar"));
+    }
+
+    /**
+     * Test jar with deflated entries.
+     */
+    @Test
+    public void loadsDeflatedJarWithManyNestedEntriesAndDoesNotUseMuchMemory() {
+        loadsJarWithManyNestedEntriesAndDoesNotUseMuchMemory(
+                Issue400.class.getClassLoader().getResource("issue400-nested-deflated.jar"));
+    }
+
+    /**
      * @return used JVM heap size allocated in RAM
-     * @see <a href="https://stackoverflow.com/a/42567450/253468">What are Runtime.getRuntime().totalMemory() and freeMemory()?</a>
+     * @see <a href="https://stackoverflow.com/a/42567450/253468">What are Runtime.getRuntime().totalMemory() and
+     *      freeMemory()?</a>
      */
     private long usedRam() {
-        Runtime runtime = Runtime.getRuntime();
+        final Runtime runtime = Runtime.getRuntime();
         runtime.gc();
         System.runFinalization();
         runtime.gc();
