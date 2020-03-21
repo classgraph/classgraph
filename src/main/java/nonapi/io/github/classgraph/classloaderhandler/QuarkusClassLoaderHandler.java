dce7b397c0dca02d057c426f4882fac6132180d5
Luke Hutchison <luke.hutch@gmail.com>
2020-03-21 16:20:55 -0600

    Source > Format

--- src/main/java/nonapi/io/github/classgraph/classloaderhandler/QuarkusClassLoaderHandler.java
+++ src/main/java/nonapi/io/github/classgraph/classloaderhandler/QuarkusClassLoaderHandler.java
@@ -41,9 +41,9 @@ import nonapi.io.github.classgraph.utils.ReflectionUtils;
  * Extract classpath entries from the Quarkus ClassLoader.
  */
 class QuarkusClassLoaderHandler implements ClassLoaderHandler {
-
     // Classloader until Quarkus 1.2
     private static final String RUNTIME_CLASSLOADER = "io.quarkus.runner.RuntimeClassLoader";
+    
     // Classloader since Quarkus 1.3
     private static final String QUARKUS_CLASSLOADER = "io.quarkus.bootstrap.classloading.QuarkusClassLoader";
 
