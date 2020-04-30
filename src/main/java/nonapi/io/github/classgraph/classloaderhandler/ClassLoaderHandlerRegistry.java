59e2586b1578b221cda8c2f0804d21f13afeca74
gus <gus@needhamsoftware.com>
2020-04-30 10:26:48 -0400

    initial UnoJarClassLoaderHandler implementation

--- src/main/java/nonapi/io/github/classgraph/classloaderhandler/ClassLoaderHandlerRegistry.java
+++ src/main/java/nonapi/io/github/classgraph/classloaderhandler/ClassLoaderHandlerRegistry.java
@@ -69,7 +69,8 @@ public class ClassLoaderHandlerRegistry {
 
                     // Java 7/8 URLClassLoader support (should be second-to-last, so that subclasses of
                     // URLClassLoader are handled by more specific handlers above)
-                    new ClassLoaderHandlerRegistryEntry(URLClassLoaderHandler.class)));
+                    new ClassLoaderHandlerRegistryEntry(URLClassLoaderHandler.class),
+                    new ClassLoaderHandlerRegistryEntry(UnoJarClassLoaderHandler.class)));
 
     /** Fallback ClassLoaderHandler. */
     public static final ClassLoaderHandlerRegistryEntry FALLBACK_HANDLER = new ClassLoaderHandlerRegistryEntry(
@@ -88,6 +89,8 @@ public class ClassLoaderHandlerRegistry {
             "WEB-INF/lib/", "WEB-INF/lib-provided/",
             // OSGi
             "META-INF/lib/",
+            // uno-jar
+            "main/",
             // Tomcat and others
             "lib/",
             // Extension dir
@@ -106,9 +109,7 @@ public class ClassLoaderHandlerRegistry {
             // Spring-Boot
             "BOOT-INF/classes/",
             // Tomcat
-            "WEB-INF/classes/",
-            // UnoJar
-            "main/" //
+            "WEB-INF/classes/" //
     };
 
     // -------------------------------------------------------------------------------------------------------------
---
+++ src/main/java/nonapi/io/github/classgraph/classloaderhandler/UnoJarClassLoaderHandler.java
@@ -0,0 +1,102 @@
+/*
+ * This file is part of ClassGraph.
+ *
+ * Author: Gus Heck
+ *
+ * Hosted at: https://github.com/classgraph/classgraph
+ *
+ * --
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Needham Software LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nonapi.io.github.classgraph.classloaderhandler;
+
+import nonapi.io.github.classgraph.classpath.ClassLoaderOrder;
+import nonapi.io.github.classgraph.classpath.ClasspathOrder;
+import nonapi.io.github.classgraph.scanspec.ScanSpec;
+import nonapi.io.github.classgraph.utils.LogNode;
+import nonapi.io.github.classgraph.utils.ReflectionUtils;
+
+/** Extract classpath entries from the an UnoJar. */
+class UnoJarClassLoaderHandler implements ClassLoaderHandler {
+    /** Class cannot be constructed. */
+    private UnoJarClassLoaderHandler() {
+    }
+
+    /**
+     * Check whether this {@link ClassLoaderHandler} can handle a given {@link ClassLoader}.
+     *
+     * @param classLoaderClass
+     *            the {@link ClassLoader} class or one of its superclasses.
+     * @param log
+     *            the log
+     * @return true if this {@link ClassLoaderHandler} can handle the {@link ClassLoader}.
+     */
+    public static boolean canHandle(final Class<?> classLoaderClass, final LogNode log) {
+        return "com.needhamsoftware.unojar.JarClassLoader".equals(classLoaderClass.getName());
+    }
+
+    /**
+     * Return true if this classloader delegates to its parent.
+     *
+     * @param classLoader
+     *            the {@link ClassLoader}.
+     * @return true if this classloader delegates to its parent.
+     */
+    @SuppressWarnings("unused")
+    private static boolean isParentFirst(final ClassLoader classLoader) {
+        return true;
+    }
+
+    /**
+     * Find the {@link ClassLoader} delegation order for a {@link ClassLoader}.
+     *
+     * @param classLoader
+     *            the {@link ClassLoader} to find the order for.
+     * @param classLoaderOrder
+     *            a {@link ClassLoaderOrder} object to update.
+     * @param log
+     *            the log
+     */
+    public static void findClassLoaderOrder(final ClassLoader classLoader, final ClassLoaderOrder classLoaderOrder,
+            final LogNode log) {
+            // Use parent-first delegation order
+            classLoaderOrder.delegateTo(classLoader.getParent(), /* isParent = */ true, log);
+        classLoaderOrder.add(classLoader, log);
+    }
+
+    /**
+     * Find the classpath entries for the associated {@link ClassLoader}.
+     *
+     * @param classLoader
+     *            the {@link ClassLoader} to find the classpath entries order for.
+     * @param classpathOrder
+     *            a {@link ClasspathOrder} object to update.
+     * @param scanSpec
+     *            the {@link ScanSpec}.
+     * @param log
+     *            the log.
+     */
+    public static void findClasspathOrder(final ClassLoader classLoader, final ClasspathOrder classpathOrder,
+            final ScanSpec scanSpec, final LogNode log) {
+        classpathOrder.addClasspathEntryObject(ReflectionUtils
+                .invokeMethod(classLoader, "getOneJarPath", false), classLoader, scanSpec, log);
+    }
+}
