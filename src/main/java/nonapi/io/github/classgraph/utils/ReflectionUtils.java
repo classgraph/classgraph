1809312e9bacfa963e8c96eeee36b794efa00e20
Luke Hutchison <luke.hutch@gmail.com>
2021-09-28 16:27:44 -0600

    Use toolfactory/jvm-driver for reflection

--- src/main/java/nonapi/io/github/classgraph/utils/ReflectionUtils.java
+++ src/main/java/nonapi/io/github/classgraph/utils/ReflectionUtils.java
@@ -29,7 +29,6 @@
 package nonapi.io.github.classgraph.utils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -37,12 +36,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import io.github.toolfactory.jvm.DefaultDriver;
+import io.github.toolfactory.jvm.Driver;
+
 /** Reflection utility methods that can be used by ClassLoaderHandlers. */
 public final class ReflectionUtils {
-
-    // In JDK 9+, could use MethodHandles.privateLookupIn
-    // And then use getter lookup to get fields (which works even if there is no getter function defined):
-    // https://stackoverflow.com/q/19135218/3950982
+    /**
+     * Use jvm-driver to bypass Java visibility restrictions, security manager limitations, and strong
+     * encapsulation.
+     */
+    private static Driver reflectionDriver = new DefaultDriver();
 
     /**
      * Constructor.
@@ -71,34 +74,18 @@ public final class ReflectionUtils {
      */
     private static Object getFieldVal(final Class<?> cls, final Object obj, final String fieldName,
             final boolean throwException) throws IllegalArgumentException {
-        Field field = null;
-        for (Class<?> currClass = cls; currClass != null; currClass = currClass.getSuperclass()) {
-            try {
-                field = currClass.getDeclaredField(fieldName);
-                // Field found
-                break;
-            } catch (final ReflectiveOperationException | SecurityException e) {
-                // Try parent
-            }
-        }
-        if (field == null) {
-            if (throwException) {
-                throw new IllegalArgumentException((obj == null ? "Static field " : "Field ") + "\"" + fieldName
-                        + "\" not found or not accessible");
-            }
-        } else {
-            try {
-                field.setAccessible(true);
-            } catch (final RuntimeException e) { // JDK 9+: InaccessibleObjectException | SecurityException
-                // Ignore
-            }
-            try {
-                return field.get(obj);
-            } catch (final IllegalAccessException e) {
-                if (throwException) {
-                    throw new IllegalArgumentException(
-                            "Can't read " + (obj == null ? "static " : "") + " field \"" + fieldName + "\": " + e);
+        try {
+            for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
+                for (Field field : reflectionDriver.getDeclaredFields(c)) {
+                    if (field.getName().equals(fieldName)) {
+                        return reflectionDriver.getFieldValue(obj, field);
+                    }
                 }
+            }
+        } catch (Exception e) {
+            if (throwException) {
+                throw new IllegalArgumentException(
+                        "Can't read " + (obj == null ? "static " : "") + " field \"" + fieldName + "\": " + e);
             }
         }
         return null;
@@ -161,116 +148,44 @@ public final class ReflectionUtils {
     }
 
     /**
-     * Iterate through implemented interfaces, top-down, then superclass to subclasses, top-down (since higher-up
-     * superclasses and superinterfaces have the highest chance of being visible).
+     * Get order in which to attempt invoking methods.
      *
      * @param cls
      *            the class
-     * @return the reverse of the order in which method calls would be attempted by the JRE.
+     * @return the order in which method calls would be attempted by the JRE.
      */
-    private static List<Class<?>> getReverseMethodAttemptOrder(final Class<?> cls) {
-        final List<Class<?>> reverseAttemptOrder = new ArrayList<>();
-
-        // Iterate from class to its superclasses
-        for (Class<?> c = cls; c != null && c != Object.class; c = c.getSuperclass()) {
-            reverseAttemptOrder.add(c);
-        }
-
-        // Find interfaces and superinterfaces implemented by this class or its superclasses
-        final Set<Class<?>> addedIfaces = new HashSet<>();
-        final LinkedList<Class<?>> ifaceQueue = new LinkedList<>();
+    private static List<Method> enumerateMethods(final Class<?> cls) {
+        // Iterate from class to its superclasses, and find initial interfaces to start traversing from
+        final List<Method> methodOrder = new ArrayList<>();
+        final Set<Class<?>> visited = new HashSet<>();
+        final LinkedList<Class<?>> interfaceQueue = new LinkedList<>();
         for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
-            if (c.isInterface() && addedIfaces.add(c)) {
-                ifaceQueue.add(c);
+            for (Method m : reflectionDriver.getDeclaredMethods(c)) {
+                methodOrder.add(m);
+            }
+            // Find interfaces and superinterfaces implemented by this class or its superclasses
+            if (c.isInterface() && visited.add(c)) {
+                interfaceQueue.add(c);
             }
             for (final Class<?> iface : c.getInterfaces()) {
-                if (addedIfaces.add(iface)) {
-                    ifaceQueue.add(iface);
+                if (visited.add(iface)) {
+                    interfaceQueue.add(iface);
                 }
             }
         }
-        while (!ifaceQueue.isEmpty()) {
-            final Class<?> iface = ifaceQueue.remove();
-            reverseAttemptOrder.add(iface);
-            final Class<?>[] superIfaces = iface.getInterfaces();
-            if (superIfaces.length > 0) {
-                for (final Class<?> superIface : superIfaces) {
-                    if (addedIfaces.add(superIface)) {
-                        ifaceQueue.add(superIface);
-                    }
+        // Traverse through interfaces looking for default methods
+        while (!interfaceQueue.isEmpty()) {
+            final Class<?> iface = interfaceQueue.remove();
+            for (Method m : reflectionDriver.getDeclaredMethods(iface)) {
+                methodOrder.add(m);
+            }
+            for (final Class<?> superIface : iface.getInterfaces()) {
+                if (visited.add(superIface)) {
+                    interfaceQueue.add(superIface);
                 }
             }
         }
-        return reverseAttemptOrder;
-    }
-
-    /**
-     * Invoke the named method in the given object or its superclasses. If an exception is thrown while trying to
-     * call the method, and throwException is true, then IllegalArgumentException is thrown wrapping the cause,
-     * otherwise this will return null. If passed a null object, returns null unless throwException is true, then
-     * throws IllegalArgumentException.
-     *
-     * @param cls
-     *            The class.
-     * @param obj
-     *            The object, or null to invoke a static method.
-     * @param methodName
-     *            The method name.
-     * @param oneArg
-     *            If true, look for a method with one argument of type argType. If false, look for method with no
-     *            arguments.
-     * @param argType
-     *            The type of the first argument to the method.
-     * @param param
-     *            The value of the first parameter to invoke the method with.
-     * @param throwException
-     *            If true, throw an exception if the field value could not be read.
-     * @return The result of the method invocation.
-     * @throws IllegalArgumentException
-     *             If the field value could not be read.
-     */
-    private static Object invokeMethod(final Class<?> cls, final Object obj, final String methodName,
-            final boolean oneArg, final Class<?> argType, final Object param, final boolean throwException)
-            throws IllegalArgumentException {
-        Method method = null;
-        final List<Class<?>> reverseAttemptOrder = getReverseMethodAttemptOrder(cls);
-        for (int i = reverseAttemptOrder.size() - 1; i >= 0; i--) {
-            final Class<?> classOrInterface = reverseAttemptOrder.get(i);
-            try {
-                method = oneArg ? classOrInterface.getDeclaredMethod(methodName, argType)
-                        : classOrInterface.getDeclaredMethod(methodName);
-                // Method found
-                break;
-            } catch (final ReflectiveOperationException | SecurityException e) {
-                // Try next interface or superclass 
-            }
-        }
-        if (method == null) {
-            if (throwException) {
-                throw new IllegalArgumentException((obj == null ? "Static method " : "Method ") + "\"" + methodName
-                        + "\" not found or not accesible");
-            }
-        } else {
-            try {
-                method.setAccessible(true);
-            } catch (final RuntimeException e) { // JDK 9+: InaccessibleObjectException | SecurityException
-                // Ignore
-            }
-            try {
-                return oneArg ? method.invoke(obj, param) : method.invoke(obj);
-            } catch (final IllegalAccessException | SecurityException e) {
-                if (throwException) {
-                    throw new IllegalArgumentException(
-                            "Can't call " + (obj == null ? "static " : "") + "method \"" + methodName + "\": " + e);
-                }
-            } catch (final InvocationTargetException e) {
-                if (throwException) {
-                    throw new IllegalArgumentException("Exception while invoking " + (obj == null ? "static " : "")
-                            + "method \"" + methodName + "\"", e);
-                }
-            }
-        }
-        return null;
+        return methodOrder;
     }
 
     /**
@@ -293,12 +208,23 @@ public final class ReflectionUtils {
             throws IllegalArgumentException {
         if (obj == null || methodName == null) {
             if (throwException) {
-                throw new NullPointerException();
+                throw new IllegalArgumentException("Unexpected null argument");
             } else {
                 return null;
             }
         }
-        return invokeMethod(obj.getClass(), obj, methodName, false, null, null, throwException);
+        try {
+            for (Method m : enumerateMethods(obj.getClass())) {
+                if (m.getName().equals(methodName) && m.getParameterTypes().length == 0) {
+                    return reflectionDriver.invoke(m, obj, new Object[0]);
+                }
+            }
+        } catch (Exception e) {
+            if (throwException) {
+                throw new IllegalArgumentException("Method \"" + methodName + "\" could not be invoked: " + e);
+            }
+        }
+        return null;
     }
 
     /**
@@ -323,14 +249,26 @@ public final class ReflectionUtils {
      */
     public static Object invokeMethod(final Object obj, final String methodName, final Class<?> argType,
             final Object param, final boolean throwException) throws IllegalArgumentException {
-        if (obj == null || methodName == null) {
+        if (obj == null || methodName == null || argType == null) {
             if (throwException) {
-                throw new NullPointerException();
+                throw new IllegalArgumentException("Unexpected null argument");
             } else {
                 return null;
             }
         }
-        return invokeMethod(obj.getClass(), obj, methodName, true, argType, param, throwException);
+        try {
+            for (Method m : enumerateMethods(obj.getClass())) {
+                if (m.getName().equals(methodName) && m.getParameterTypes().length == 1
+                        && m.getParameterTypes()[0] == argType) {
+                    return reflectionDriver.invoke(m, obj, new Object[] { param });
+                }
+            }
+        } catch (Exception e) {
+            if (throwException) {
+                throw new IllegalArgumentException("Method \"" + methodName + "\" could not be invoked: " + e);
+            }
+        }
+        return null;
     }
 
     /**
@@ -353,12 +291,23 @@ public final class ReflectionUtils {
             final boolean throwException) throws IllegalArgumentException {
         if (cls == null || methodName == null) {
             if (throwException) {
-                throw new NullPointerException();
+                throw new IllegalArgumentException("Unexpected null argument");
             } else {
                 return null;
             }
         }
-        return invokeMethod(cls, null, methodName, false, null, null, throwException);
+        try {
+            for (Method m : enumerateMethods(cls)) {
+                if (m.getName().equals(methodName) && m.getParameterTypes().length == 0) {
+                    return reflectionDriver.invoke(m, null, new Object[0]);
+                }
+            }
+        } catch (Exception e) {
+            if (throwException) {
+                throw new IllegalArgumentException("Method \"" + methodName + "\" could not be invoked: " + e);
+            }
+        }
+        return null;
     }
 
     /**
@@ -383,14 +332,26 @@ public final class ReflectionUtils {
      */
     public static Object invokeStaticMethod(final Class<?> cls, final String methodName, final Class<?> argType,
             final Object param, final boolean throwException) throws IllegalArgumentException {
-        if (cls == null || methodName == null) {
+        if (cls == null || methodName == null || argType == null) {
             if (throwException) {
-                throw new NullPointerException();
+                throw new IllegalArgumentException("Unexpected null argument");
             } else {
                 return null;
             }
         }
-        return invokeMethod(cls, null, methodName, true, argType, param, throwException);
+        try {
+            for (Method m : enumerateMethods(cls)) {
+                if (m.getName().equals(methodName) && m.getParameterTypes().length == 1
+                        && m.getParameterTypes()[0] == argType) {
+                    return reflectionDriver.invoke(m, null, new Object[] { param });
+                }
+            }
+        } catch (Exception e) {
+            if (throwException) {
+                throw new IllegalArgumentException("Method \"" + methodName + "\" could not be invoked: " + e);
+            }
+        }
+        return null;
     }
 
     /**
